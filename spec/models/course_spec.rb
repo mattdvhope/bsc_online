16d4a924753ff47c37937510d1da20ed73bff0ca
mattdvhope <mattagape@me.com>
2014-12-09 20:26:09 +0700

    Added spec for belong to curriculum

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -5,25 +5,25 @@ describe Course do
   it { should belong_to(:curriculum) }
 
   it "saves itself when activated" do
-    course = Course.new(name: "Government English", activated: true)
+    course = Fabricate(:course, activated: true)
     course.save
     expect(Course.count).to eq(1)
   end
 
   it "saves itself when not activated" do
-    course = Course.new(name: "Government English", activated: false)
+    course = Fabricate(:course)
     course.save
     expect(Course.count).to eq(1)
   end
 
   it "#activated? is true when activated" do
-    course = Course.new(name: "Government English", activated: true)
+    course = Fabricate(:course, activated: true)
     course.save
     expect(course.activated?).to eq(true)
   end
 
   it "#activated? is false when not activated" do
-    course = Course.new(name: "Government English", activated: false)
+    course = Fabricate(:course)
     course.save
     expect(course.activated?).to eq(false)
   end
