54897931b303f0384315f17cfa20636220add895
mattdvhope <mattagape@me.com>
2014-12-09 17:13:05 +0700

    Attempted work on postal_code, but then it reverted

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,45 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141204052623) do
+ActiveRecord::Schema.define(version: 20141208081413) do
 
   create_table "courses", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.boolean  "activated"
+  end
+
+  create_table "curriculums", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "course_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "description"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.string   "address_1"
+    t.string   "address_2"
+    t.string   "city"
+    t.string   "sub_district"
+    t.string   "district"
+    t.string   "province"
+    t.integer  "postal_code",              limit: 255
+    t.string   "country"
+    t.string   "phone_number"
+    t.integer  "age"
+    t.string   "gender"
+    t.string   "occupation"
+    t.string   "university_name"
+    t.string   "religion"
+    t.boolean  "studied_english_before?"
+    t.integer  "studied_english_how_long"
+    t.boolean  "interested_in_follow_up?"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
