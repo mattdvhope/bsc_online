e4383b843312dfc2461dacd49db9511ed53157b2
mattdvhope <mattagape@me.com>
2014-12-11 15:10:43 +0700

    Added require_user and current_user methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def require_user
+    redirect_to sign_in_path unless current_user
+  end
+
+  def current_user
+    User.find(session[:user_id]) if session[:user_id] # Need this conditional to protect from a 'nil' error.
+  end
+
+  helper_method :current_user # This makes the 'current_user' method available in the views.
+
 end
