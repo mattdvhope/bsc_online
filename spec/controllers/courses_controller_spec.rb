8c2354dbb155ab985c7ea54467ec34e4212746d6
mattdvhope <mattagape@me.com>
2014-12-17 23:35:10 +0700

    Further testing on this controller

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe CoursesController, :type => :controller do
+describe CoursesController do
+
+  describe "GET index" do
+    it "sets the @curriculums all the curriculums" do
+      curriculum1 = Fabricate(:curriculum)
+      curriculum2 = Fabricate(:curriculum)
+      curriculums = [curriculum1, curriculum2]
+      get :index
+      expect(assigns(:curriculums)).to match_array(curriculums)
+    end
+  end
+
+  describe "GET show" do
+    it "sets @course" do
+      course = Fabricate(:course)
+      get :show, id: course.id
+      expect(assigns(:course)).to eq(course)
+    end
+  end
 
 end
