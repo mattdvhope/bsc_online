6cfdd78f139fe6fe2091e76abde2a82739b6b279
mattdvhope <mattagape@me.com>
2014-12-17 23:29:12 +0700

    Added info about student users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141211075526) do
+ActiveRecord::Schema.define(version: 20141217032422) do
 
   create_table "courses", force: true do |t|
     t.string   "name"
@@ -26,6 +26,16 @@ ActiveRecord::Schema.define(version: 20141211075526) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "description"
+    t.string   "name"
+  end
+
+  create_table "syllabuses", force: true do |t|
+    t.integer  "curriculum_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "title"
+    t.string   "description"
+    t.integer  "student_id"
   end
 
   create_table "users", force: true do |t|
@@ -52,7 +62,6 @@ ActiveRecord::Schema.define(version: 20141211075526) do
     t.boolean  "interested_in_follow_up?"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "curriculum_id"
   end
 
 end
