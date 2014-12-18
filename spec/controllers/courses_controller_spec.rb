d5a06acea9253cd7c68429f53f2075ca681e9b92
mattdvhope <mattagape@me.com>
2014-12-18 12:17:19 +0700

    Removed index method

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -2,20 +2,11 @@ require 'rails_helper'
 
 describe CoursesController do
 
-  describe "GET index" do
-    it "sets the @curriculums all the curriculums" do
-      curriculum1 = Fabricate(:curriculum)
-      curriculum2 = Fabricate(:curriculum)
-      curriculums = [curriculum1, curriculum2]
-      get :index
-      expect(assigns(:curriculums)).to match_array(curriculums)
-    end
-  end
-
   describe "GET show" do
     it "sets @course" do
+      curriculum = Fabricate(:curriculum)
       course = Fabricate(:course)
-      get :show, id: course.id
+      get :show, curriculum_id: curriculum.id, id: course.id 
       expect(assigns(:course)).to eq(course)
     end
   end
