7990072ca16b1178df60d8cbf8905a0ecc77456d
mattdvhope <mattagape@me.com>
2014-12-19 17:32:14 +0700

    Deleted Syllabus files and replaced them with Plan

--- app/models/syllabus.rb
+++
@@ -1,6 +0,0 @@
-class Syllabus < ActiveRecord::Base
-
-  belongs_to :curriculum
-  belongs_to :student, class_name: "User"
-
-end
--- spec/models/syllabus_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-describe Syllabus do
-
-  it { should belong_to(:curriculum) }
-  it { should belong_to(:student).class_name('User') }
-
-end
