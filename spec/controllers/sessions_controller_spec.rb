c14c1651532a272f13836d0485204ea15f89b2ad
mattdvhope <mattagape@me.com>
2014-12-22 16:54:37 +0700

    In the midst of setting up registration for new courses - might involve this file

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,19 +1,24 @@
 require 'rails_helper'
 
 describe SessionsController do
+
   describe "GET new" do
+
     it "renders the new template for unauthenticated users" do
       get :new
       expect(response).to render_template :new
     end
+
     it "redirects to home path/page if the user is authenticated" do
       set_current_user
       get :new
       expect(response).to redirect_to home_path
     end
+    
   end
 
   describe "POST create" do
+
     context "user able to sign in / valid credentials" do
       before {
         alice = Fabricate(:user)
@@ -31,6 +36,7 @@ describe SessionsController do
         expect(flash[:success]).not_to be_blank # We expect "something" to be there with a flash notice.
       end
     end
+
     context "user unable to sign in / invalid credentials" do
       before do
         alice = Fabricate(:user)
@@ -46,23 +52,29 @@ describe SessionsController do
         expect(flash[:danger]).not_to be_blank
       end
     end
+
   end
 
   describe "GET destroy" do
+
     before do
       # session[:user_id] = Fabricate(:user).id
       set_current_user
       get :destroy
     end
+
     it "clears the session for the user" do
       expect(session[:user_id]).to be_nil
     end
+
     it "redirects to to the root path" do
       expect(response).to redirect_to root_path
     end
+
     it "sets the flash success" do
       expect(flash[:success]).not_to be_blank
     end
+
   end
 
 end
