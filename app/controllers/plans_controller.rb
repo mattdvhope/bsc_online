7de9a5fac6f535ddb3fac535b8c56ec9dc10d119
mattdvhope <mattagape@me.com>
2014-12-26 10:16:26 +0700

    Worked on index action -- redirecting

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -2,15 +2,6 @@ class PlansController < ApplicationController
 
   before_action :require_user
 
-  def index
-    @plans = current_user.plans
-    if current_user.plans.empty?
-      redirect_to new_plan_path 
-    else
-      render 'index'
-    end
-  end
-
   def new
     @plan = Plan.new
     @curriculums = Curriculum.all
@@ -18,6 +9,16 @@ class PlansController < ApplicationController
 
   def create
     
+  end
+
+  def index
+    @curriculums = Curriculum.all
+    @plans = current_user.plans
+    if current_user.plans.empty?
+      redirect_to new_plan_path 
+    else
+      render 'index'
+    end
   end
 
   def show
