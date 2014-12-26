c9c83637a15dfd2653e15d07c09bf8d5bc7d9b98
mattdvhope <mattagape@me.com>
2014-12-26 11:50:04 +0700

    Changed from sign in to log in

--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-feature 'User signs in' do
+feature 'User logs in' do
 
   background do
     @alice = Fabricate(:user, email: "alice@test.tv")
   end
 
   scenario "with existing email address" do
-    visit sign_in_path
+    visit log_in_path
     fill_in "Email Address", with: "alice@test.tv" # To 'fill_in', you can use the field-name (in the users table) or the label tag ('Email Address', in this case [which is BEST PRACTICE]) which is on the 'sessions/new.html.haml' page; Or, you can use an id.  Capybara will read any of the three.
     fill_in "Password", with: "password"
     click_button "Let's study English!"
