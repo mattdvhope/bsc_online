e8335a4d69ae5d3734237d749d95f7cf876b11c9
mattdvhope <mattagape@me.com>
2014-12-26 12:54:23 +0700

    Added spec for non-user

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe PagesController, :type => :controller do
+describe PagesController do
+
+  describe "GET front" do
+    it "redirects to home path if current user" do
+      set_current_user
+      get :front
+      expect(response).to redirect_to home_path
+    end
+
+    it "render front page if not current user" do
+      get :front
+      expect(response).to render_template("front")
+    end
+
+    it "sets the @curriculums all the curriculums" do
+      curriculum1 = Fabricate(:curriculum)
+      curriculum2 = Fabricate(:curriculum)
+      curriculums = [curriculum1, curriculum2]
+      get :front
+      expect(assigns(:curriculums)).to match_array(curriculums)
+    end
+  end
 
 end
