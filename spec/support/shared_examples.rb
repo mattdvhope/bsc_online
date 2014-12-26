d2a73f92333e31b2ae312d31c61285b6492a03cd
mattdvhope <mattagape@me.com>
2014-12-26 11:50:40 +0700

    Changed from sign in to log in

--- spec/support/shared_examples.rb
+++ spec/support/shared_examples.rb
@@ -1,7 +1,7 @@
-shared_examples "requires sign in" do
-  it "redirects to the sign in page" do
+shared_examples "requires log in" do
+  it "redirects to the log in page" do
     session[:user_id] = nil
     action # example of action... get :new
-    expect(response).to redirect_to sign_in_path # If I ever want to change my sign_in_path I now only have to do so here--not throughout every place where I do "requires sign in".
+    expect(response).to redirect_to log_in_path # If I ever want to change my log_in_path I now only have to do so here--not throughout every place where I do "requires sign in".
   end
 end
