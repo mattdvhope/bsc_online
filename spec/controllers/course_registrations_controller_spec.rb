e846e6525ef53bd9b03a8412447324d885f2f499
mattdvhope <mattagape@me.com>
2014-12-26 10:11:57 +0700

    Added course registrations action pack and routes and specs

--- spec/controllers/course_registrations_controller_spec.rb
+++ spec/controllers/course_registrations_controller_spec.rb
@@ -7,18 +7,21 @@ describe CourseRegistrationsController do
       let(:action) { get :new }
     end
 
-    it "renders course registration template for authenticated users" do
+    it "renders template of courses available for registering" do
       set_current_user
       get :new
       expect(response).to render_template :new
     end
+  end
 
-    # it "redirects to home path/page if the user is authenticated" do
-    #   set_current_user
-    #   get :new
-    #   expect(response).to redirect_to home_path
-    # end
-
+  describe "GET index" do
+    it "sets the @curriculums all the curriculums" do
+      curriculum1 = Fabricate(:curriculum)
+      curriculum2 = Fabricate(:curriculum)
+      curriculums = [curriculum1, curriculum2]
+      get :index
+      expect(assigns(:curriculums)).to match_array(curriculums)
+    end
   end
 
 end
