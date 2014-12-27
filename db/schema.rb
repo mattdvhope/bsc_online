96085cb6afc82cd6ef72aed8b5c6e8acc7802f3e
mattdvhope <mattagape@me.com>
2014-12-28 05:24:15 +0700

    Added guest column to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141227112009) do
+ActiveRecord::Schema.define(version: 20141227215257) do
 
   create_table "conversations", force: true do |t|
     t.integer  "lesson_id"
@@ -145,6 +145,7 @@ ActiveRecord::Schema.define(version: 20141227112009) do
     t.boolean  "interested_in_follow_up?"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "guest"
   end
 
 end
