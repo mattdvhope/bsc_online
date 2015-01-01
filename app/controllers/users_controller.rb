3f628f11836a61284f88a317513a355a651fa5d4
mattdvhope <mattagape@me.com>
2015-01-01 08:52:03 +0700

    If guest then becomes new user, his guest plans will transfer to him and guest object destroyed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,18 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
-    @courses = Course.all
   end
 
   def create
-    @user = User.new(user_params)
+    @user = params.include?("user") ? User.new(user_params) : User.new_guest
     if @user.save
-      flash[:success] = "You now have a 'member account' with BSC English Online. Welcome aboard!"
+      if @user.first_name.blank?
+        flash[:success] = "You are logged in as a temporary guest. Please be aware that any work you do while logged in as a 'temporary guest' will not be recorded after you have logged out. But if you decide to Join BSC English Online, all your work from this time will be retained."
+      else
+        @user.plans = current_user.plans # Guest user's plans transferred to created user before guest user is destroyed.
+        current_user.destroy # guest user record destroyed
+        flash[:success] = "You now have a 'member account' with BSC English Online. Welcome aboard!"
+      end
       session[:user_id] = @user.id
       redirect_to root_path
     else
