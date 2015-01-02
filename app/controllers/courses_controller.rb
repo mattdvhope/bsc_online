b3c51e942240c0fce7ee313d7816210e9b8e36f7
mattdvhope <mattagape@me.com>
2015-01-02 13:36:47 +0700

    Added require user for show

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,6 @@
 class CoursesController < ApplicationController
 
-  before_action :require_user, :only => [:edit]
+  before_action :require_user, :only => [:show, :edit]
 
   def show
     @course = Course.find(1)
