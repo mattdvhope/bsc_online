65719bd8e3c7b395cea93206564b47b711bb134d
mattdvhope <mattagape@me.com>
2015-01-02 21:45:50 +0700

    Tried changes to deal with spec failure, but then went back to original

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,7 +32,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false # Make this 'false' when using: gem 'database_cleaner' and add features from Avdi Grimm's blog. Also, after adding gem 'database_cleaner', you may have to delete some vcr cassettes b/c js (maybe) was not true before.
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -61,7 +61,6 @@ RSpec.configure do |config|
     DatabaseCleaner.clean
   end
 
-
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
