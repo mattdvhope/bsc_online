51cdd41e43687a5bdee0a1567547a09617ede08b
mattdvhope <mattagape@me.com>
2015-01-05 14:10:04 +0700

    Deleted extra conversation controllers

--- app/controllers/conversation_english_zero_thais_controller.rb
+++
@@ -1,8 +0,0 @@
-class ConversationEnglishZeroThaisController < ApplicationController
-
-  def show
-    @conversation = Conversation.find(params[:id])
-    @conversation_english_zero_thai = ConversationEnglishZeroThai.find(params[:id])
-  end
-
-end
--- app/controllers/conversation_hundred_thais_controller.rb
+++
@@ -1,8 +0,0 @@
-class ConversationHundredThaisController < ApplicationController
-
-  def show
-    @conversation = Conversation.find(params[:id])
-    @conversation_hundred_thai = ConversationHundredThai.find(params[:id])
-  end
-
-end
