71003a498f16862d234f1bd10ae5b69ec7ff1143
mattdvhope <mattagape@me.com>
2015-01-05 16:50:07 +0700

    Removed specs for non-DRY models

--- spec/models/conversation_hundred_thai_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-describe ConversationHundredThai do
-
-  it { should belong_to(:conversation) }
-
-end
--- spec/models/text_english_zero_thai_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-describe TextEnglishZeroThai do
-
-  it { should belong_to(:story) }
-
-end
