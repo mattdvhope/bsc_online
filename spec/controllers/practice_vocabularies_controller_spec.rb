50d39e14a4e2fc144e57fc7b34308320d2399062
mattdvhope <mattagape@me.com>
2015-01-05 16:47:59 +0700

    Removed specs for non-DRY controllers

--- spec/controllers/practice_vocabularies_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe PracticeVocabulariesController, :type => :controller do
-
-end
--- spec/controllers/text_english_zero_thais_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe TextEnglishZeroThaisController, :type => :controller do
-
-end
