86558fae9eda68e838c768966305b87516f4d7ac
mattdvhope <mattagape@me.com>
2015-01-05 16:14:45 +0700

    Added index action

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,5 +1,10 @@
 class ConversationsController < ApplicationController
 
+  def index
+    @lesson = Lesson.find(params[:lesson_id])
+    @conversations = @lesson.conversations
+  end
+
   def show
     @conversation = Conversation.find(params[:id])
   end
