b6df2a2ba8e250835b7c670bd2c495adda3d0ace
mattdvhope <mattagape@me.com>
2015-01-05 14:16:18 +0700

    Removed extra story controllers

--- app/controllers/text_english_fifty_thais_controller.rb
+++
@@ -1,8 +0,0 @@
-class TextEnglishFiftyThaisController < ApplicationController
-
-  def show
-    @story = Story.find(params[:id])
-    @text_english_fifty_thai = TextEnglishFiftyThai.find(params[:id])
-  end
-
-end
--- app/controllers/text_english_seventy_thais_controller.rb
+++
@@ -1,8 +0,0 @@
-class TextEnglishSeventyThaisController < ApplicationController
-
-  def show
-    @story = Story.find(params[:id])
-    @text_english_seventy_thai = TextEnglishSeventyThai.find(params[:id])
-  end
-
-end
