88a0551d08e2ad650b29975a92fd701ff526194f
mattdvhope <mattagape@me.com>
2015-01-05 15:53:17 +0700

    Took out extra sub-stories

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,10 +1,5 @@
 class Story < ActiveRecord::Base
 
   belongs_to :lesson
-  has_many :text_hundred_thais
-  has_many :text_english_seventy_thais
-  has_many :text_english_fifty_thais
-  has_many :text_english_twenty_thais
-  has_many :text_english_zero_thais
 
 end
