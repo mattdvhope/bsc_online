aded1c099b7419787016ffeefce4bd0452a93357
mattdvhope <mattagape@me.com>
2015-01-07 12:19:03 +0700

    Removed specs for relationships that had been deleted

--- spec/models/practice_spec.rb
+++ spec/models/practice_spec.rb
@@ -3,8 +3,5 @@ require 'rails_helper'
 describe Practice do
 
   it { should belong_to(:lesson) }
-  it { should have_many(:practice_vocabularies) }
-  it { should have_many(:practice_phrases) }
-  it { should have_many(:practice_sentences) }
 
 end
