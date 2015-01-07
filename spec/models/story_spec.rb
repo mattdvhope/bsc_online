8b2bb47c47f7e00a3654fbb705cf46d10ef410b6
mattdvhope <mattagape@me.com>
2015-01-07 12:19:03 +0700

    Removed specs for relationships that had been deleted

--- spec/models/story_spec.rb
+++ spec/models/story_spec.rb
@@ -3,10 +3,5 @@ require 'rails_helper'
 describe Story do
 
   it { should belong_to(:lesson) }
-  it { should have_many(:text_hundred_thais) }
-  it { should have_many(:text_english_seventy_thais) }
-  it { should have_many(:text_english_fifty_thais) }
-  it { should have_many(:text_english_twenty_thais) }
-  it { should have_many(:text_english_zero_thais) }
 
 end
