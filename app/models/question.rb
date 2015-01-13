618ce183426deb5f654978177fd8133a1768e26c
mattdvhope <mattagape@me.com>
2015-01-13 17:11:05 +0700

    Added app/models/question.rb method

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,4 +5,12 @@ class Question < ActiveRecord::Base
 
   accepts_nested_attributes_for :answers, reject_if: lambda { |a| a[:answer_content].blank? }, allow_destroy: true
 
+  def verify_one_correct_answer
+    correct_answers = []
+    self.answers.each do |answer|
+      correct_answers << answer if answer.correct?
+    end
+    return correct_answers
+  end
+
 end
