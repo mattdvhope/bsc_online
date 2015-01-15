a538bf3cd57304dc2e11b6c6a41ba46bd28b476d
mattdvhope <mattagape@me.com>
2015-01-15 14:21:40 +0700

    Removed user_answers; replaced with student_answers

--- db/migrate/20150109223947_create_user_answers.rb
+++
@@ -1,9 +0,0 @@
-class CreateUserAnswers < ActiveRecord::Migration
-  def change
-    create_table :user_answers do |t|
-      t.integer :student_id
-      t.integer :answer_id
-      t.timestamps
-    end
-  end
-end
