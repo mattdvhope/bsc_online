a52bf14280ced796deb6b853c57a7716a5b7ba5b
mattdvhope <mattagape@me.com>
2015-01-19 06:28:03 +0700

    Added choice field to answers

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150115034301) do
+ActiveRecord::Schema.define(version: 20150116052209) do
 
   create_table "answers", force: :cascade do |t|
     t.integer  "question_id"
     t.text     "answer_content"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "correct?"
+    t.string   "choice"
+    t.boolean  "correct"
   end
 
   create_table "assessments", force: :cascade do |t|
