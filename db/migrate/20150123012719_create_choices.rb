58bef94faa3d84aecfeb985fa7c4ea4ce5f83296
mattdvhope <mattagape@me.com>
2015-01-23 18:22:12 +0700

    Changed out choices again

--- db/migrate/20150123012719_create_choices.rb
+++
@@ -1,9 +0,0 @@
-class CreateChoices < ActiveRecord::Migration
-  def change
-    create_table :choices do |t|
-      t.boolean :selected
-      t.references :selectable, polymorphic: true, index: true
-      t.timestamps null: false
-    end
-  end
-end
