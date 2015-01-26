ce4d222ca77ab3b424f92c0e12a980176db3d279
mattdvhope <mattagape@me.com>
2015-01-26 16:52:50 +0700

    Set up spec for instantiate_new_choices_for_all_answers_for_new_student method

--- spec/controllers/assessments_controller_spec.rb
+++ spec/controllers/assessments_controller_spec.rb
@@ -2,35 +2,40 @@ require 'rails_helper'
 
 describe AssessmentsController do
 
-  describe "GET new" do
+  describe "GET show" do # I need an @assessment with an id for this spec to work.
+
+  I must instantiate an Assessment here first.
 
     it_behaves_like "requires log in" do
-      let(:action) { get :new }
+      let(:action) { get :show, id: assessment.id }
     end
 
-    it "sets the @assessment instance variable" do
+    it "renders the show template" do
       set_current_user
-      get :new
-      expect(assigns(:assessment)).to be_instance_of(Assessment)
+      get :show, id: assessment.id
+      expect(response).to render_template(:show)
     end
-
 
   end
 
-  # describe "GET show" do # I need an @assessment with an id for this spec to work.
+  describe "GET new" do
 
-  # I must instantiate an Assessment here first.
+    it_behaves_like "requires log in" do
+      let(:action) { get :new, { :curriculum_id => 1, :course_id => 1 } }
+    end
 
-  #   it_behaves_like "requires log in" do
-  #     let(:action) { get :show, id: assessment.id }
-  #   end
+    it "sets the @course for this assessment" do
+      set_current_user
+      get :new, { :curriculum_id => 1, :course_id => 1 }
+      expect(assigns(:course)).to be_instance_of Course
+    end
 
-  #   it "renders the show template" do
-  #     set_current_user
-  #     get :show, id: assessment.id
-  #     expect(response).to render_template(:show)
-  #   end
+    it "sets the @assessment to a new assessment with course_id" do
+      set_current_user
+      get :new, { :curriculum_id => 1, :course_id => 1 }
+      expect(assigns(:assessment)).to be_instance_of(Assessment)
+    end
 
-  # end
+  end
 
 end
