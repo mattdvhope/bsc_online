c544db472856dd7b2c7d4daa8531625fd73486a6
mattdvhope <mattagape@me.com>
2015-01-27 13:25:33 +0700

    Updated spec and removed unneeded methods in model

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 describe Question do
 
   it { should belong_to(:assessment) }
-  it { should have_many(:answers) }
+  it { should have_many(:answers).dependent(:destroy) }
+  it { should have_many(:choices).through(:answers) }
+
+  it { should accept_nested_attributes_for :answers }
 
 end
