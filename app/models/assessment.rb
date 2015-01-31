9127153f579c5beab0d8e1124a707207362d6c09
mattdvhope <mattagape@me.com>
2015-01-31 12:25:02 +0700

    Tweaks, but then switched back to original

--- app/models/assessment.rb
+++ app/models/assessment.rb
@@ -7,7 +7,7 @@ class Assessment < ActiveRecord::Base
 
   accepts_nested_attributes_for :questions, reject_if: proc { |attributes| attributes['question_content'].blank? }, allow_destroy: true
 
-  def instantiate_new_choices_for_all_answers_for_new_student(student)
+  def instantiate_new_choices_for_all_answers_for_new_questions(student)
     self.questions.each do |question|
       question.answers.each do |answer|
         answer.choices << Choice.new(answer_id: answer.id, student_id: student.id, selected: false)
