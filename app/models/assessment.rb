5d41e6f0aba415a96e9434042510881d0eb901c6
mattdvhope <mattagape@me.com>
2015-01-31 21:53:40 +0700

    Set up validation and further testing for assessment

--- app/models/assessment.rb
+++ app/models/assessment.rb
@@ -7,6 +7,18 @@ class Assessment < ActiveRecord::Base
 
   accepts_nested_attributes_for :questions, reject_if: proc { |attributes| attributes['question_content'].blank? }, allow_destroy: true
 
+  validates_presence_of :part_id, :unless => :an_exam?
+  validates_presence_of :lesson_id, :unless => :a_test?, :unless => :an_exam?
+  validates_presence_of :type_of 
+
+  def a_test?
+    self.type_of == "Test"
+  end
+
+  def an_exam?
+    self.type_of == "Exam"
+  end
+
   def instantiate_new_choices_for_all_answers_for_new_questions(student)
     self.questions.each do |question|
       question.answers.each do |answer|
