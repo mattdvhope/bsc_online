aa86b73b1884f8b6e96ec66cbfb2af3e9fd8775e
mattdvhope <mattagape@me.com>
2015-01-31 14:30:46 +0700

    Added set_current_overseer

--- spec/support/macros.rb
+++ spec/support/macros.rb
@@ -1,4 +1,7 @@
 def set_current_user(user=nil)
   session[:user_id] = (user || Fabricate(:user)).id # assigns ‘.id’ to either one
 end # The '||' allows the option of either having ‘alice’ or not having ‘alice’.
-  
+
+def set_current_overseer(name="Admin")
+  session[:user_id] = Fabricate(:role, name: name).overseer.id
+end
