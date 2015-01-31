0ae1c5b88c1a92f1dc9bb85e0666b6386a90ccbe
mattdvhope <mattagape@me.com>
2015-01-31 14:53:28 +0700

    Completed specs for index and new

--- spec/controllers/admin/assessments_controller_spec.rb
+++ spec/controllers/admin/assessments_controller_spec.rb
@@ -4,18 +4,20 @@ describe Admin::AssessmentsController do
 
   describe "GET index" do
 
-    it_behaves_like "requires log in" do
-      let(:action) { 
-        assessment = Fabricate(:assessment)
-        get :index, curriculum_id: assessment.course.curriculum.id, course_id: assessment.course.id, id: assessment.id
-      }
+    it_behaves_like "requires overseer" do
+      let(:action) { get :new, { :curriculum_id => 1, :course_id => 1 } }
     end
 
-    it "renders the index template" do
-      set_current_user
+    it "sets @assessments to all assessments" do
+      set_current_overseer
       test = Fabricate(:assessment)
-      quiz = Fabricate(:assessment)
-binding.pry
+      get :index, { :curriculum_id => 1, :course_id => 1 }
+      expect(assigns(:assessments)).to eq([test])
+    end
+
+    it "renders the :index view" do
+      set_current_overseer
+      get :index, { :curriculum_id => 1, :course_id => 1 }
       expect(response).to render_template :index
     end
 
@@ -28,18 +30,27 @@ binding.pry
     end
 
     it "sets the @course for this assessment" do
-      set_current_user
-      get :new, { :curriculum_id => 1, :course_id => 1 }
+      set_current_overseer
+      course = Fabricate(:course)
+      get :new, { :curriculum_id => course.curriculum.id, :course_id => course.id }
       expect(assigns(:course)).to be_instance_of Course
     end
 
     it "sets the @assessment to a new assessment with course_id" do
-      set_current_user
-      get :new, { :curriculum_id => 1, :course_id => 1 }
+      set_current_overseer
+      course = Fabricate(:course)
+      get :new, { :curriculum_id => course.curriculum.id, :course_id => course.id }
       expect(assigns(:assessment)).to be_instance_of(Assessment)
       expect(assigns(:assessment)).to be_new_record 
     end
 
+  end
+
+  describe "POST create" do
+    context "with valid input" do
+
+      
+    end
   end
 
 end
