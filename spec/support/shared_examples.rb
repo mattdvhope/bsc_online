8f410c319dd1fcc55ebc928941c77de06b5c220a
mattdvhope <mattagape@me.com>
2015-01-31 14:53:58 +0700

    Added shared example for requires overseer

--- spec/support/shared_examples.rb
+++ spec/support/shared_examples.rb
@@ -1,7 +1,15 @@
 shared_examples "requires log in" do
   it "redirects to the log in page" do
     session[:user_id] = nil
-    action # example of action... get :new
+    action
     expect(response).to redirect_to log_in_path # If I ever want to change my log_in_path I now only have to do so here--not throughout every place where I do "requires sign in".
+  end
+end
+
+shared_examples "requires overseer" do
+  it "flashes that the user is not authorized to do that" do
+    session[:user_id] = Fabricate(:user).id
+    action
+    expect(flash[:danger]).to eq "You are not authorized to do that."
   end
 end
