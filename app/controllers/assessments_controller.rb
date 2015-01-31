e910c9da7ee8312b49fb57128e0a00d3f129079d
mattdvhope <mattagape@me.com>
2015-01-31 12:43:23 +0700

    Commented out conditional for spec, and then uncommented it again

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -4,9 +4,9 @@ class AssessmentsController < ApplicationController
 
   def show
     @assessment = Assessment.find(params[:id])
-    # if @assessment.questions.first.choices.size == 0
+    if @assessment.questions.first.choices.size == 0
       @assessment.instantiate_new_choices_for_all_answers_for_new_questions(current_user)
-    # end
+    end
   end
 
 end
