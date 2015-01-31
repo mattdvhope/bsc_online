35730f883f9f2c0054f16a8adb40dffb1acc94b3
mattdvhope <mattagape@me.com>
2015-01-31 08:05:45 +0700

    Added spec for reject_if

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -8,4 +8,7 @@ describe Question do
 
   it { should accept_nested_attributes_for :answers }
 
+  anaf_for_answers = Question.nested_attributes_options[:answers]
+  it { expect(anaf_for_answers[:reject_if].call({ "answer_content" => "" })).to eq(true) }
+
 end
