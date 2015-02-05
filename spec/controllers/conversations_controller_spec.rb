a49d1008e3f982a963e9d797f90136743d5e4dee
mattdvhope <mattagape@me.com>
2015-02-05 12:57:33 +0700

    Made specs for conversations controller

--- spec/controllers/conversations_controller_spec.rb
+++ spec/controllers/conversations_controller_spec.rb
@@ -1,5 +1,71 @@
 require 'rails_helper'
 
-RSpec.describe ConversationsController, :type => :controller do
+describe ConversationsController do
+
+  describe "GET index" do
+
+    it_behaves_like "requires log in" do
+      let(:action) {
+        part = Part.create
+        lesson = Lesson.create(part_id: part.id)
+        get :index, lesson_id: lesson.id, part_id: part.id
+      }
+    end
+
+    before :each do
+      @part = Part.create
+      @lesson = Lesson.create(part_id: @part.id)
+      @conversation = Conversation.create(lesson_id: @lesson.id)
+      set_current_user
+    end
+
+    it "sets @lesson" do
+      get :index, lesson_id: @lesson.id, part_id: @part.id
+      expect(assigns(:lesson)).to be_instance_of Lesson
+    end
+
+    it "sets @conversations" do
+      get :index, lesson_id: @lesson.id, part_id: @part.id
+      expect(assigns(:conversations)).to eq [@conversation]
+    end
+
+  end
+
+  describe "GET show" do
+
+    it_behaves_like "requires log in" do
+      let(:action) {
+        part = Part.create
+        lesson = Lesson.create(part_id: part.id)
+        get :show, id: 1, lesson_id: lesson.id, part_id: part.id
+      }
+    end
+
+    it "sets @conversation" do
+      part = Part.create
+      lesson = Lesson.create(part_id: part.id)
+      conversation = Conversation.create(lesson_id: lesson.id)
+      set_current_user
+      get :show, id: conversation.id, lesson_id: lesson.id, part_id: part.id
+      expect(assigns(:conversation)).to be_instance_of Conversation
+    end
+
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 end
