3996fed826fd63d8307428f609f4c5a935fbbf0d
mattdvhope <mattagape@me.com>
2015-02-05 15:46:21 +0700

    Added validation for student id

--- spec/models/plan_spec.rb
+++ spec/models/plan_spec.rb
@@ -6,6 +6,7 @@ describe Plan do
   it { should belong_to(:student).class_name('User') }
 
   it { should validate_presence_of(:curriculum_id) }
+  it { should validate_presence_of(:student_id) }
   it { should validate_presence_of(:description) }
 
 end
