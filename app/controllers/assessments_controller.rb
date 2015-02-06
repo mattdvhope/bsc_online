282b231aeedb3388a82079c1a73ecc9e3affbca6
mattdvhope <mattagape@me.com>
2015-02-06 07:01:14 +0700

    Redid the logic for deciding whether to instantiate new choices for new student; revised specs accordingly

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -4,12 +4,14 @@ class AssessmentsController < ApplicationController
 
   def show
     @assessment = Assessment.find(params[:id])
-    if @assessment.questions.size == 0
+    if @assessment.questions.size == 0 # This 'if' is only for when the admin first creates an assessment.  It's not for the regular user/student.
       flash[:danger] = "You need to put questions & answers in. Try again."
       @assessment.destroy
       redirect_to new_curriculum_course_admin_assessment_path
-    elsif @assessment.questions.first.choices.size == 0
-      @assessment.instantiate_new_choices_for_all_answers_for_new_student(current_user)
+    else # This 'else' is for the regular student/user using an already-created assessment.
+      if @assessment.questions.first.answers.first.choices.where(student_id: current_user.id).count == 0
+        @assessment.instantiate_new_choices_for_all_answers_for_new_student(current_user)
+      end
     end
   end
 
--- spec/controllers/assessments_controller_spec.rb
+++ spec/controllers/assessments_controller_spec.rb
@@ -36,42 +36,31 @@ describe AssessmentsController do
 
     end
 
-    context "first time the assessment show page is shown" do
+    context "the assessment has questions" do # A correctly built assessment exists here.
 
-      it "instantiates new choices for all answers for new student if the question has no choices yet" do
-        set_current_user
-        answer = Fabricate(:answer)
-        assessment = answer.question.assessment
-        get :show, curriculum_id: assessment.course.curriculum.id, course_id: assessment.course.id, id: assessment.id
-        expect(assessment.questions.first.choices.size).to be > 0
+      before :each do
+        @answer = Fabricate(:answer) # also a question, assessment, course & curriculum automatically fabricated
+        @question = @answer.question
+        @assessment = @answer.question.assessment
+        @course = @answer.question.assessment.course
+        @curriculum = @answer.question.assessment.course.curriculum
       end
 
-    end
-
-    context "subsequent times the assessment show page is shown" do
-
-      it "does not instantiate new choices for all answers for new student if the question has choices" do
+      it "renders the show template and instantiates new choices for all answers for new student" do
         alice = Fabricate(:user)
         set_current_user(alice)
-        answer = Fabricate(:answer)
-        assessment = answer.question.assessment
-        get :show, curriculum_id: assessment.course.curriculum.id, course_id: assessment.course.id, id: assessment.id
-        get :show, curriculum_id: assessment.course.curriculum.id, course_id: assessment.course.id, id: assessment.id
-        expect(assessment.questions.first.choices.size).to eq 1
+        get :show, curriculum_id: @curriculum.id, course_id: @course.id, id: @assessment.id
+        expect(response).to render_template(:show)
+        expect(alice.choices.count).to eq 1
       end
 
-    end
-
-    context "the assessment has questions" do
-
-      it "renders the show template" do
-        assessment = Fabricate(:assessment)
-        set_current_user
-        question = Fabricate(:question)
-        question.answers << Fabricate(:answer)
-        assessment.questions << question
-        get :show, curriculum_id: assessment.course.curriculum.id, course_id: assessment.course.id, id: assessment.id
+      it "renders the show template and does not instantiate new choices for returning student" do
+        alice = Fabricate(:user)
+        set_current_user(alice)
+        get :show, curriculum_id: @curriculum.id, course_id: @course.id, id: @assessment.id
+        get :show, curriculum_id: @curriculum.id, course_id: @course.id, id: @assessment.id
         expect(response).to render_template(:show)
+        expect(alice.choices.count).to eq 1 # not 2!!
       end
 
     end
