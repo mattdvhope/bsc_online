b97151cfeb476e68d3a26aebe0776a168a333908
mattdvhope <mattagape@me.com>
2015-02-06 11:00:21 +0700

    Completed specs for sessions controller; particularly the #destroy action

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -102,20 +102,4 @@ describe SessionsController do
 
   end
 
-  # describe "GET destroy" do
-  #   before do
-  #     set_current_user
-  #     get :destroy
-  #   end
-  #   it "clears the session for the user" do
-  #     expect(session[:user_id]).to be_nil
-  #   end
-  #   it "redirects to to the root path" do
-  #     expect(response).to redirect_to root_path
-  #   end
-  #   it "sets the notice" do
-  #     expect(flash[:notice]).not_to be_blank
-  #   end
-  # end
-
 end
