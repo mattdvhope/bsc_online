497200849b5f63a8385bd9f577760c1194a3a162
mattdvhope <mattagape@me.com>
2015-02-11 17:38:19 +0700

    Added has many answers through questions

--- app/models/assessment.rb
+++ app/models/assessment.rb
@@ -4,6 +4,7 @@ class Assessment < ActiveRecord::Base
   belongs_to :part
   belongs_to :lesson
   has_many :questions, :dependent => :destroy
+  has_many :answers, :through => :questions
 
   accepts_nested_attributes_for :questions, reject_if: proc { |attributes| attributes['question_content'].blank? }, allow_destroy: true
 
