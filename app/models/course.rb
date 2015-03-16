48592ba01ec69d1e1bd3c5c707fb415246977b09
mattdvhope <mattagape@me.com>
2015-03-17 05:00:25 +0700

    Added validations and show_exam method

--- app/models/course.rb
+++ app/models/course.rb
@@ -5,6 +5,9 @@ class Course < ActiveRecord::Base
   has_many :lessons, through: :parts
   has_many :assessments
 
+  validates_presence_of :name, :description, :curriculum_id
+  validates_uniqueness_of :name, :description
+
   def activated?
     self.activated == true
   end
