709950f647f58056ea5205499231eb2f3d66f4d1
mattdvhope <mattagape@me.com>
2015-03-17 12:57:44 +0700

    Included AssessmentProvidable module

--- lib/assessment_providable.rb
+++ lib/assessment_providable.rb
@@ -1,25 +1,52 @@
 module AssessmentProvidable
 
-  def show_exam
+  def show_assessment
     if self.assessments.count > 0
-      self.assessments.each do |assessment|
-        if assessment.type_of == "Exam"
-          return assessment.type_of
-        else
-          return nil
+      case self.class.name
+      when "Course"
+        self.assessments.each do |assessment|
+          if assessment.type_of == "Exam"
+            return assessment.type_of
+          end
+        end
+      when "Part"
+        self.assessments.each do |assessment|
+          if assessment.type_of == "Test"
+            return assessment.type_of
+          end
+        end
+      when "Lesson"
+        self.assessments.each do |assessment|
+          if assessment.type_of == "Quiz"
+            return assessment.type_of
+          end
         end
       end
     end
     nil
   end
 
-  def provide_exam_object
-    self.assessments.each do |assessment|
-      if assessment.type_of == "Exam"
-        return assessment
+  def provide_assessment_object
+    case self.class.name
+    when "Course"
+      self.assessments.each do |assessment|
+        if assessment.type_of == "Exam"
+          return assessment
+        end
+      end
+    when "Part"
+      self.assessments.each do |assessment|
+        if assessment.type_of == "Test"
+          return assessment
+        end
+      end
+    when "Lesson"
+      self.assessments.each do |assessment|
+        if assessment.type_of == "Quiz"
+          return assessment
+        end
       end
     end
-    nil
   end
 
 end
