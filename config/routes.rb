e544eaa49f923611c568cab2b4e2b69ec82a4a0f
mattdvhope <mattagape@me.com>
2015-03-17 12:59:31 +0700

    Added namespace :admin for parts and lessons

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,14 @@ Rails.application.routes.draw do
     resources :courses, only: [:new, :create, :edit, :update]
   end
 
+  namespace :admin do
+    resources :parts, only: [:new, :create, :edit, :update]
+  end
+
+  namespace :admin do
+    resources :lessons, only: [:new, :create, :edit, :update]
+  end
+
   resources :parts, only: [:show] do
     resources :lessons, only: [:show] do
       with_options only: [:index, :show] do |list_only|
