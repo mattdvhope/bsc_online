bdd3a52fa443187ba6ca5f81511dff061954b653
mattdvhope <mattagape@me.com>
2015-03-19 15:41:14 +0700

    Set up grades including business logic for determining a student's score on an assessment

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -4,15 +4,30 @@ class AssessmentsController < ApplicationController
 
   def show
     @assessment = Assessment.find(params[:id])
-    if @assessment.questions.size == 0 # This 'if' is only for when the admin first creates an assessment.  It's not for the regular user/student.
+    @grade = grade_for_view(@assessment)
+    if @assessment.questions.size == 0 # This 'if & else' is only for when the admin first creates an assessment.  It's not for the regular user/student.
       flash[:danger] = "You need to put questions & answers in. Try again."
       @assessment.destroy
       redirect_to new_curriculum_course_admin_assessment_path
-    else # This 'else' is for during the test-building process of admin user.
+    else
       if @assessment.answers_of_student_not_all_chosen_yet?(current_user)
         @assessment.instantiate_new_choices_for_all_answers_for_new_student(current_user)
       end
     end
   end
+
+  private
+
+    def grade_for_view(assessment)
+      if !assessment.provide_grade_object_for_this_assessment(current_user)
+        make_grade_object_for_this_assessment_for_this_student(assessment)
+      else
+        assessment.provide_grade_object_for_this_assessment(current_user)
+      end
+    end
+
+    def make_grade_object_for_this_assessment_for_this_student(assessment)
+      Grade.create(student_id: current_user.id, assessment_id: assessment.id)
+    end
 
 end
--- app/models/grade.rb
+++ app/models/grade.rb
@@ -3,4 +3,25 @@ class Grade < ActiveRecord::Base
   belongs_to :student, class_name: "User"
   belongs_to :assessment
 
+  def student_score_for_assessment(student)
+    number_correct = self.number_of_questions_answered_correctly(student)
+    number_of_questions = self.assessment.questions.count
+
+    # You might need to round up the float before this b/c '#score' is an integer -- not a float!
+    self.score = ((number_correct.to_f / number_of_questions.to_f) * 100).round(2)
+  end
+
+  def number_of_questions_answered_correctly(student)
+    questions_answered_correctly = 0
+    self.assessment.questions.each do |question|
+      question.answers.each do |answer|
+        answer_selected = answer.choices.where(student_id: student.id).first.selected
+        if answer.choice == "Correct" && answer_selected == true
+          questions_answered_correctly = questions_answered_correctly + 1
+        end
+      end
+    end
+    questions_answered_correctly
+  end
+
 end
