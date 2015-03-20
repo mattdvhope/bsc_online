3f4ef7096ef2f1643ab23d3db84edf00926fa46f
mattdvhope <mattagape@me.com>
2015-03-20 14:16:08 +0700

    Set up lesson, assessment and grade models, controllers and views with more logic associated w/ assessments

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -3,6 +3,8 @@ class LessonsController < ApplicationController
   def show
     @lesson = Lesson.find(params[:id])
     @assessment = @lesson.provide_assessment_object
+    @assessment.make_sure_choices_are_instantiated(current_user)
+# binding.pry
   end
 
 end
