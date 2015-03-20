7d4d5ff3b28f6e33ed1607718bf642cfb6b27044
mattdvhope <mattagape@me.com>
2015-03-20 14:16:08 +0700

    Set up lesson, assessment and grade models, controllers and views with more logic associated w/ assessments

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -5,14 +5,10 @@ class AssessmentsController < ApplicationController
   def show
     @assessment = Assessment.find(params[:id])
     @grade = grade_for_view(@assessment)
-    if @assessment.questions.size == 0 # This 'if & else' is only for when the admin first creates an assessment.  It's not for the regular user/student.
+    if @assessment.questions.size == 0 # This 'if' is only for when the admin first creates an assessment.  It's not for the regular user/student.
       flash[:danger] = "You need to put questions & answers in. Try again."
       @assessment.destroy
       redirect_to new_curriculum_course_admin_assessment_path
-    else
-      if @assessment.answers_of_student_not_all_chosen_yet?(current_user)
-        @assessment.instantiate_new_choices_for_all_answers_for_new_student(current_user)
-      end
     end
   end
 
