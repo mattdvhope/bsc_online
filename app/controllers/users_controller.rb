5e612339723380091dd5ce92ae663c9825883359
mattdvhope <mattagape@me.com>
2015-03-23 17:14:10 +0700

    Made transition_to_student_status_if_a_guest_in_app method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,12 +10,10 @@ class UsersController < ApplicationController
       if @user.guest
         flash[:success] = "You are logged in as a temporary guest. Please be aware that any work you do while logged in as a 'temporary guest' will not be recorded after you have logged out. But if you decide to Join BSC English Online (while still logged in this time!), all your work from this time will be retained."
       else
-        @user.plans = current_user.plans if current_user # Retains 'plans' of Guest User if he decides to Register.
-        @user.choices = current_user.choices if current_user # Retains choices of Guest User if he decides to Register.
-        current_user.destroy if current_user # guest?
+        transition_to_student_status_if_a_guest_in_app(@user)
         flash[:success] = "You now have a 'member account' with BSC English Online, #{@user.first_name}. Welcome aboard!"
       end
-      session[:user_id] = @user.id # This allows 'guest' to enter app rather than being redirected to the log_in page.
+      session[:user_id] = @user.id
       redirect_to home_path
     else
       flash[:danger] = "You were not able to Sign Up"
@@ -25,8 +23,15 @@ class UsersController < ApplicationController
 
   private
 
-  def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :postal_code, :address_1, :address_2, :city, :sub_district, :district, :province, :country, :phone_number, :age, :gender, :occupation, :university_name, :religion, :studied_english_before?, :studied_english_how_long, :interested_in_follow_up?, :guest)
-  end
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :postal_code, :address_1, :address_2, :city, :sub_district, :district, :province, :country, :phone_number, :age, :gender, :occupation, :university_name, :religion, :studied_english_before?, :studied_english_how_long, :interested_in_follow_up?, :guest)
+    end
+
+    def transition_to_student_status_if_a_guest_in_app(user)
+      user.plans = current_user.plans if current_user # guest?
+      user.choices = current_user.choices if current_user # guest?
+      user.grades = current_user.grades if current_user # guest?
+      current_user.destroy if current_user # guest?
+    end
 
 end
