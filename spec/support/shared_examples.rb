56ac9d4f780d7e6e1959924323425ae659828b3c
mattdvhope <mattagape@me.com>
2015-03-25 15:11:41 +0700

    Changed first one to redirects to root path rather than to log in page

--- spec/support/shared_examples.rb
+++ spec/support/shared_examples.rb
@@ -1,5 +1,5 @@
 shared_examples "requires log in" do
-  it "redirects to the log in page" do
+  it "redirects to the root path" do
     session[:user_id] = nil
     action
     expect(response).to redirect_to root_path # If I ever want to change my root_path I now only have to do so here--not throughout every place where I do "requires sign in".
