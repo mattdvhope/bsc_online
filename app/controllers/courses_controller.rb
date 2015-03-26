eb9266a5a554d1341ee92e660a1bbf10d0892cfa
mattdvhope <mattagape@me.com>
2015-03-26 10:57:59 +0700

    Added provision of @assessment and specs for that

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -7,6 +7,10 @@ class CoursesController < ApplicationController
     if @course.id != 1
       render 'under_construction'
     end
+    @assessment = @course.provide_assessment_object # method from lib/assessment_providable.rb
+    if @assessment
+      @assessment.make_sure_choices_are_instantiated(current_user)
+    end
   end
 
 end
