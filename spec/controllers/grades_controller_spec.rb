c3d2e6979fe133660631d3eabaf3b7c62adc031a
mattdvhope <mattagape@me.com>
2015-03-26 13:31:03 +0700

    Made specs for patch update

--- spec/controllers/grades_controller_spec.rb
+++ spec/controllers/grades_controller_spec.rb
@@ -1,5 +1,60 @@
 require 'rails_helper'
 
-RSpec.describe GradesController, :type => :controller do
+describe GradesController do
+
+  it_behaves_like "requires log in" do
+    let(:action) {
+      assessment = Fabricate(:assessment)
+      alice = Fabricate(:user)
+      grade = Grade.create(student_id: alice.id, assessment_id: assessment.id)
+      get :show, id: grade.id
+    }
+  end
+
+  describe "GET index" do
+  end
+
+  describe "GET show" do
+
+    it "sets @grade" do
+      assessment = Fabricate(:assessment)
+      alice = Fabricate(:user)
+      set_current_user(alice)
+      grade = Grade.create(student_id: alice.id, assessment_id: assessment.id)
+      get :show, id: grade.id
+      expect(assigns(:grade)).to be_instance_of Grade
+    end
+
+  end
+
+  describe "PATCH update" do
+
+    it "sets @grade" do
+      assessment = Fabricate(:assessment)
+      alice = Fabricate(:user)
+      set_current_user(alice)
+      grade = Grade.create(student_id: alice.id, assessment_id: assessment.id)
+      patch :update, id: grade.id
+      expect(assigns(:grade)).to be_instance_of Grade
+    end
+
+    it "it gives the score for @grade" do
+      alice = Fabricate(:user)
+      set_current_user(alice)
+      answer = Fabricate(:answer)
+      question = Fabricate(:question)
+      question.answers << answer
+      assessment = Fabricate(:assessment)
+      assessment.questions << question
+      choice = Choice.create(answer_id: answer.id, student_id: alice.id)
+      choice.selected = true
+      answer.choices << choice
+      grade = Grade.create(student_id: alice.id, assessment_id: assessment.id)
+      patch :update, id: grade.id
+      grade.reload
+      expect(grade.score).not_to eq nil
+    end
+
+  end
 
 end
