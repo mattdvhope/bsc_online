125bb77fce8cbb691d3af5e491cd2be0592100dd
mattdvhope <mattagape@me.com>
2015-03-28 12:30:02 +0700

    Moved choices js out

--- app/assets/javascripts/assessments.js
+++ app/assets/javascripts/assessments.js
@@ -2,26 +2,3 @@
 // All this logic will automatically be available in application.js.
 // You can use CoffeeScript in this file: http://coffeescript.org/
 
-// $(document).ready(function () {
-
-//   // alert("I am an alert box!");
-
-//   $( "a.answer" ).mouseenter(
-//     function() {
-//       $( this ).addClass( "hover_over" );
-//     }
-//   );
-
-//   $( "a.answer" ).mouseleave(
-//     function() {
-//       $( this ).removeClass( "hover_over" );
-//     }
-//   );
-
-//   $( "a.answer" ).on( "click", function() {
-
-//     alert("I am an alert box!");
-
-//   });
-
-// });
