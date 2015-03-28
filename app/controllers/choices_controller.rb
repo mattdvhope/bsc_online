3b91504a36288e916c68c933620b2ee68beda446
mattdvhope <mattagape@me.com>
2015-03-28 13:01:53 +0700

    Removed unneeded line

--- app/controllers/choices_controller.rb
+++ app/controllers/choices_controller.rb
@@ -5,7 +5,6 @@ class ChoicesController < ApplicationController
     @choices = @choice.question.choices.where(student_id: current_user.id)
     @choices.each do |choice|
       choice.update_column(:selected, false)
-      choice.answer
     end
     @choice.update_column(:selected, true)
   end
