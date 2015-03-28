43e4ce0ba15373bf8d4aea5bb22b022e939af2dc
mattdvhope <mattagape@me.com>
2015-03-28 12:33:45 +0700

    Removed respond to js

--- app/controllers/choices_controller.rb
+++ app/controllers/choices_controller.rb
@@ -1,17 +1,13 @@
 class ChoicesController < ApplicationController
 
   def update
-    respond_to do |format|
-      format.js   {
-        @choice = Choice.find(params[:id])
-        @choices = @choice.question.choices.where(student_id: current_user.id)
-        @choices.each do |choice|
-          choice.update_column(:selected, false)
-          choice.answer
-        end
-        @choice.update_column(:selected, true)
-      }
+    @choice = Choice.find(params[:id])
+    @choices = @choice.question.choices.where(student_id: current_user.id)
+    @choices.each do |choice|
+      choice.update_column(:selected, false)
+      choice.answer
     end
+    @choice.update_column(:selected, true)
   end
 
 end
