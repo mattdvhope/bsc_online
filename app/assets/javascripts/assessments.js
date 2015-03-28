2b7dbba1dbfa5b7c070ac2ffcbb6b4d822a7bc27
mattdvhope <mattagape@me.com>
2015-03-28 09:50:40 +0700

    Replaced coffeescript with js

---
+++ app/assets/javascripts/assessments.js
@@ -0,0 +1,27 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+// You can use CoffeeScript in this file: http://coffeescript.org/
+
+// $(document).ready(function () {
+
+//   // alert("I am an alert box!");
+
+//   $( "a.answer" ).mouseenter(
+//     function() {
+//       $( this ).addClass( "hover_over" );
+//     }
+//   );
+
+//   $( "a.answer" ).mouseleave(
+//     function() {
+//       $( this ).removeClass( "hover_over" );
+//     }
+//   );
+
+//   $( "a.answer" ).on( "click", function() {
+
+//     alert("I am an alert box!");
+
+//   });
+
+// });
