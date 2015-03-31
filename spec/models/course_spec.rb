23ce2c912039226be1ebc85b2852f869c0ecc8e9
mattdvhope <mattagape@me.com>
2015-03-31 10:07:27 +0700

    Added validation specs

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -7,6 +7,12 @@ describe Course do
   it { should have_many(:lessons).through(:parts) }
   it { should have_many(:assessments) }
 
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:description) } 
+  it { should validate_presence_of(:curriculum_id) }
+  it { should validate_uniqueness_of(:name) }
+  it { should validate_uniqueness_of(:description) }
+
   it "#activated? is true when activated" do
     course = Fabricate(:course, activated: true)
     course.save
