bb20d4c75759bf09e3206b5eef83a8bac365a6e4
mattdvhope <mattagape@me.com>
2015-03-31 10:07:27 +0700

    Added validation specs

--- spec/models/part_spec.rb
+++ spec/models/part_spec.rb
@@ -6,4 +6,8 @@ describe Part do
   it { should have_many(:lessons) }
   it { should have_many(:assessments) }
 
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:course_id) }
+  it { should validate_uniqueness_of(:name) }
+
 end
