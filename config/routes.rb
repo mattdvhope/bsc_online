a5e3bd3d277697687e5788c61aee04469f9d47d3
mattdvhope <mattagape@me.com>
2015-05-19 06:06:28 +0700

    Added edit and update to assessments for student

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   resources :curriculums, only: [:index, :show] do
     resources :courses, only: [:show] do
-      resources :assessments, only: [:show]
+      resources :assessments, only: [:show, :edit, :update]
       namespace :admin do
         resources :assessments, only: [:index, :new, :create, :edit, :update]
       end
