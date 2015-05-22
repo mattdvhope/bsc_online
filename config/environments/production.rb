23fbf0187c207a3ae6ffefbcc91c3819b94e5845
mattdvhope <mattagape@me.com>
2015-05-22 16:03:52 +0700

    Removed config for uglifier false

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  # To use Angular in production
-  config.assets.js_compressor = Uglifier.new(mangle: false)
 end
