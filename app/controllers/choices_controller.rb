b2cb96b504d0ec53c04e902f6d6c138b9b691256
mattdvhope <mattagape@me.com>
2015-05-27 15:37:29 +0700

    Removed quiz app angular code; began modifying Assessment choices for json

--- app/controllers/choices_controller.rb
+++ app/controllers/choices_controller.rb
@@ -1,13 +1,17 @@
 class ChoicesController < ApplicationController
 
   def update
-    @choice = Choice.find(params[:id])
-    @choices = @choice.question.hash_of_answers_and_student_choices(current_user)
-    @choice.question.answers.each do |answer|
-      choice = @choices[answer]
-      choice.update_column(:selected, false)
+    respond_to do |format|
+      format.json { 
+        @choice = Choice.find(params[:id])
+        @choices = @choice.question.hash_of_answers_and_student_choices(current_user)
+        @choice.question.answers.each do |answer|
+          choice = @choices[answer]
+          choice.update_column(:selected, false)
+        end
+        @choice.update_column(:selected, true)
+      }
     end
-    @choice.update_column(:selected, true)
   end
 
 end
