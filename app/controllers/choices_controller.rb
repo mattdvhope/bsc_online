75a9a16aac3b258a9be1689273f21565d6d7d03c
mattdvhope <mattagape@me.com>
2015-05-28 08:34:31 +0700

    Changed syntax for respond to json

--- app/controllers/choices_controller.rb
+++ app/controllers/choices_controller.rb
@@ -1,17 +1,15 @@
 class ChoicesController < ApplicationController
 
+  respond_to :json
+
   def update
-    respond_to do |format|
-      format.json { 
-        @choice = Choice.find(params[:id])
-        @choices = @choice.question.hash_of_answers_and_student_choices(current_user)
-        @choice.question.answers.each do |answer|
-          choice = @choices[answer]
-          choice.update_column(:selected, false)
-        end
-        @choice.update_column(:selected, true)
-      }
+    @choice = Choice.find(params[:id])
+    @choices = @choice.question.hash_of_answers_and_student_choices(current_user)
+    @choice.question.answers.each do |answer|
+      choice = @choices[answer]
+      choice.update_column(:selected, false)
     end
+      @choice.update_column(:selected, true)
   end
 
 end
