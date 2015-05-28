dc098ac8e601f9b6afc480660f49e52dbf0a3746
mattdvhope <mattagape@me.com>
2015-05-28 12:42:25 +0700

    Made respond to json for all actions

--- app/controllers/choices_controller.rb
+++ app/controllers/choices_controller.rb
@@ -2,14 +2,18 @@ class ChoicesController < ApplicationController
 
   respond_to :json
 
+  def show
+    respond_with Choice.find(params[:id])
+  end
+
   def update
     @choice = Choice.find(params[:id])
     @choices = @choice.question.hash_of_answers_and_student_choices(current_user)
     @choice.question.answers.each do |answer|
       choice = @choices[answer]
-      choice.update_column(:selected, false)
+      respond_with choice.update_column(:selected, false)
     end
-      @choice.update_column(:selected, true)
+      respond_with @choice.update_column(:selected, true)
   end
 
 end
