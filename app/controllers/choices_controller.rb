ce9f88cb09c9f6ac089ffa74886b54ecb6d81a4a
mattdvhope <mattagape@me.com>
2015-05-31 06:17:12 +0700

    Worked on update action to connect it the the Angular app

--- app/controllers/choices_controller.rb
+++ app/controllers/choices_controller.rb
@@ -2,18 +2,27 @@ class ChoicesController < ApplicationController
 
   respond_to :json
 
+  def index
+    choices = Choice.all.where(student_id: current_user.id)
+    respond_with choices
+  end
+
   def show
     respond_with Choice.find(params[:id])
   end
 
   def update
     @choice = Choice.find(params[:id])
-    @choices = @choice.question.hash_of_answers_and_student_choices(current_user)
-    @choice.question.answers.each do |answer|
-      choice = @choices[answer]
-      respond_with choice.update_column(:selected, false)
-    end
-      respond_with @choice.update_column(:selected, true)
+    @choice.selected = true
+    @choice.save
+# binding.pry
+    # @choice = Choice.find(params[:id])
+    # @choices = @choice.question.hash_of_answers_and_student_choices(current_user)
+    # @choice.question.answers.each do |answer|
+    #   choice = @choices[answer]
+    #   choice.update_column(:selected, false)
+    # end
+    #   @choice.update_column(:selected, true)
   end
 
 end
