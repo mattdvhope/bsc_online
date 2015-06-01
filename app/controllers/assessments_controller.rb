eddc46926cba2d10c77eb9d5b48606820d343b96
mattdvhope <mattagape@me.com>
2015-06-01 16:22:09 +0700

    More work on Angular assessment app

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -2,15 +2,18 @@ class AssessmentsController < ApplicationController
 
   before_action :require_user
 
+  # respond_to :json
+
   def show
     @assessment = Assessment.find(params[:id])
-    @answers_choices = @assessment.hash_of_answers_and_student_choices(current_user)
-    @grade = grade_for_assessment_completed_button(@assessment)
-    if @assessment.questions.size == 0 # This 'if' is only for when the admin works on an assessment.  It's not for the regular user/student.
-      flash[:danger] = "You need to put questions & answers in. Try again."
-      @assessment.destroy
-      redirect_to new_curriculum_course_admin_assessment_path
-    end
+    respond_with @assessment
+    # @answers_choices = @assessment.hash_of_answers_and_student_choices(current_user)
+    # @grade = grade_for_assessment_completed_button(@assessment)
+    # if @assessment.questions.size == 0 # This 'if' is only for when the admin works on an assessment.  It's not for the regular user/student.
+    #   flash[:danger] = "You need to put questions & answers in. Try again."
+    #   @assessment.destroy
+    #   redirect_to new_curriculum_course_admin_assessment_path
+    # end
   end
 
   private
