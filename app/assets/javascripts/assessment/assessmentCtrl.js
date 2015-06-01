c0eb9f48ad19f78f3a76389b543cb7fededc4568
mattdvhope <mattagape@me.com>
2015-06-01 14:18:36 +0700

    Started work on Angular app, assessment

---
+++ app/assets/javascripts/assessment/assessmentCtrl.js
@@ -0,0 +1,23 @@
+angular.module('assessment')
+
+.controller('AssessmentCtrl', [
+'$scope',
+function($scope){
+  // $scope.posts = posts.posts;
+
+  // $scope.addPost = function(){
+  //   if(!$scope.title || $scope.title === '') { return; }
+
+  //   posts.create({
+  //     title: $scope.title,
+  //     link: $scope.link
+  //   });
+
+  //   $scope.title = '';
+  //   $scope.link = '';
+  // };
+
+  // $scope.incrementUpvotes = function(post) {
+  //   // posts.upvote(post);
+  // };
+}]);
