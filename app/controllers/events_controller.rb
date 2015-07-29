76d83442fc3f9e7a016757027a880966f6e69d3a
mattdvhope <mattagape@me.com>
2015-07-30 05:46:08 +0700

    Set up Events for backbone app

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,11 +4,21 @@ class EventsController < ApplicationController
   respond_to :json
 
   def index
-
+    respond_with Event.all  # GET /events
   end
 
   def new
-
+    respond_with Event.new
   end
+
+  def create
+    respond_with Event.create(event_params)  # POST /events
+  end
+
+  private
+
+    def event_params
+      params.require(:event).permit(:name, :month, :date, :notes)
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
   get 'home', to: "courses#show", defaults: { id: 1 }
   get 'build', to: "pages#build"
 
-  resources :events, only: [:index, :new]
+  resources :tasks, only: :index
+
+  resources :events
+
 
   get 'sign_up', to: "users#new"
   resources :users, only: [:create]
