365e9aa55c312ea996de3f9c9297442bee1c0292
mattdvhope <mattagape@me.com>
2015-07-30 05:52:10 +0700

    Removed tasks table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150728050014) do
+ActiveRecord::Schema.define(version: 20150729224732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -145,12 +145,6 @@ ActiveRecord::Schema.define(version: 20150728050014) do
     t.string   "lesson_language_version"
     t.text     "content"
     t.string   "audio"
-  end
-
-  create_table "tasks", force: :cascade do |t|
-    t.string   "notes"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
