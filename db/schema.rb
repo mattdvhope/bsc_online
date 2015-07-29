daedec75c299ac2004deb63476d72db7d383aeac
mattdvhope <mattagape@me.com>
2015-07-30 05:46:08 +0700

    Set up Events for backbone app

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150530235731) do
+ActiveRecord::Schema.define(version: 20150728050014) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,11 +73,13 @@ ActiveRecord::Schema.define(version: 20150530235731) do
     t.string   "name"
   end
 
-  create_table "entries", force: :cascade do |t|
+  create_table "events", force: :cascade do |t|
     t.string   "name"
-    t.boolean  "winner"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "month"
+    t.integer  "date"
+    t.text     "notes"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "grades", force: :cascade do |t|
@@ -143,6 +145,12 @@ ActiveRecord::Schema.define(version: 20150530235731) do
     t.string   "lesson_language_version"
     t.text     "content"
     t.string   "audio"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string   "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
