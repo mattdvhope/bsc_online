93c92751ac3f1bcc759f012bf942bd5c9ef10347
mattdvhope <mattagape@me.com>
2015-10-14 16:40:58 +0700

    Added dependent destroy

--- app/models/part.rb
+++ app/models/part.rb
@@ -3,8 +3,8 @@ class Part < ActiveRecord::Base
   include AssessmentProvidable
 
   belongs_to :course
-  has_many :lessons
-  has_many :assessments
+  has_many :lessons, :dependent => :destroy
+  has_many :assessments, :dependent => :destroy
 
   validates_presence_of :name, :course_id
   validates_uniqueness_of :name
