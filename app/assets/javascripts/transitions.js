9897adf41d7e8d06e95e95f2bf741368f10c2170
mattdvhope <mattagape@me.com>
2015-10-29 14:11:00 +0700

    Restored scrollTop method

--- app/assets/javascripts/transitions.js
+++ app/assets/javascripts/transitions.js
@@ -10,12 +10,12 @@ $(".page-scroll").on("click", function() {
   $(this).addClass("active");
 });
 
-// $('a[href^="#"]').on('click',function (e) {
-//   e.preventDefault();
-//     $('html, body').animate({
-//       scrollTop: $($(this).attr('href')).offset().top
-//     }, 0, 'swing');
-// });
+$('a[href^="#"]').on('click',function (e) {
+  e.preventDefault();
+    $('html, body').animate({
+      scrollTop: $($(this).attr('href')).offset().top
+    }, 0, 'swing');
+});
 
 $(document).on('click','.navbar-collapse.in',function(e) {
     if( $(e.target).is('a') ) {
