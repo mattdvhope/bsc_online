93966d004a43d2dfa0a3a89d58c672f1fb440727
mattdvhope <mattagape@me.com>
2015-10-29 11:56:46 +0700

    Improved close of flash msg

--- app/assets/javascripts/transitions.js
+++ app/assets/javascripts/transitions.js
@@ -1,28 +1,38 @@
-$(document).ready(function() {
+// Scrolling up & down home page...
+$("#place_1").addClass("active");
 
-  $("#place_1").addClass("active");
-
-  $(".page-scroll").on("click", function() {
-    event.preventDefault();
-    $("#place_1").removeClass("active");
-    $("#place_2").removeClass("active");
-    $("#place_3").removeClass("active");
-    $("#place_4").removeClass("active");
-    $(this).addClass("active");
-  });
-
-  $('a[href^="#"]').on('click',function (e) {
-    e.preventDefault();
-      $('html, body').animate({
-        scrollTop: $($(this).attr('href')).offset().top
-      }, 300, 'swing');
-  });
-
-  $(document).on('click','.navbar-collapse.in',function(e) {
-      if( $(e.target).is('a') ) {
-          $(this).collapse('hide');
-      }
-  });
-
+$(".page-scroll").on("click", function() {
+  event.preventDefault();
+  $("#place_1").removeClass("active");
+  $("#place_2").removeClass("active");
+  $("#place_3").removeClass("active");
+  $("#place_4").removeClass("active");
+  $(this).addClass("active");
 });
+
+$('a[href^="#"]').on('click',function (e) {
+  e.preventDefault();
+    $('html, body').animate({
+      scrollTop: $($(this).attr('href')).offset().top
+    }, 0, 'swing');
+});
+
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') ) {
+        $(this).collapse('hide');
+    }
+});
+
+// escaping Backbone-provided boxes
+$(document).on("keyup", function(e) {
+   if (e.keyCode == 27) {
+    $('a.close').trigger('click');
+  }
+});
+
+$("a.close").on("click", function(e) {
+  e.preventDefault();
+  $(this).parent().fadeOut(1100);
+});
+
 
