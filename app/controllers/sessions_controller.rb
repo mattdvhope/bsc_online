493d6349bb8b281bc186b013438268678f73b3e3
mattdvhope <mattagape@me.com>
2015-10-29 12:53:02 +0700

    Changed redirect for invalid entries

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
       redirect_to home_path 
     else
       flash[:danger] = "Invalid email or password."
-      redirect_to log_in_path
+      redirect_to root_path
     end
   end
 
