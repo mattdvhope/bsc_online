6d86fae329348d654de910c2a0e78ab97c44d02f
mattdvhope <mattagape@me.com>
2015-11-03 16:46:06 +0700

    Set up alert for guest student

--- app/assets/javascripts/sweet_alert_code.js
+++ app/assets/javascripts/sweet_alert_code.js
@@ -1,11 +1,15 @@
 console.log(gon.user);
 
-swal({
-  title: "Thank you for visiting!",
-  text: gon.user,
-  timer: 2000,
-  showConfirmButton: true
-});
+if (gon.user) {
+  swal({
+    title: "สวัสดีครับ -- Thank you for visiting as our guest!",
+    text: "You are logged in as a temporary guest. Please be aware that any work you do while logged in as a 'temporary guest' will not be recorded after you have logged out. But if you decide to Join BSC English Online (while still logged in this time!), all your work from this time will be retained.",
+    timer: 19000,
+    showConfirmButton: true,
+    allowOutsideClick: true
+  });
+}
+
 
 
 
