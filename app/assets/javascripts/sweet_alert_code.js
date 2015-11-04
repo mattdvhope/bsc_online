e276c00d65fd47581742e9038903b58c09e7bdd9
mattdvhope <mattagape@me.com>
2015-11-04 11:14:43 +0700

    Added 'one hour' to text

--- app/assets/javascripts/sweet_alert_code.js
+++ app/assets/javascripts/sweet_alert_code.js
@@ -3,8 +3,8 @@ console.log(gon.user);
 if (gon.user) {
   swal({
     title: "สวัสดีครับ -- Thank you for visiting as our guest!",
-    text: "You are logged in as a temporary guest. Please be aware that any work you do while logged in as a 'temporary guest' will not be recorded after you have logged out. But if you decide to Join BSC English Online (while still logged in this time!), all your work from this time will be retained.",
-    timer: 19000,
+    text: "You are logged in as a temporary guest for one hour. Please be aware that any work you do while logged in as a 'temporary guest' will not be recorded after you have logged out. But if you decide to Join BSC English Online (while still logged in this time!), all your work from this time will be retained.",
+    timer: 20000,
     showConfirmButton: true,
     animation: "slide-from-bottom"
   });
