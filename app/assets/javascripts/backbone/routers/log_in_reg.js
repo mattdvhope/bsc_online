13c6401b074cd08443a633888ca1b9ad0551397d
mattdvhope <mattagape@me.com>
2015-11-10 05:46:10 +0700

    Moving Registration form to Backbone template

--- app/assets/javascripts/backbone/routers/log_in_reg.js
+++ app/assets/javascripts/backbone/routers/log_in_reg.js
@@ -1,7 +1,9 @@
 var Router = Backbone.Router.extend({
   routes: {
     "log_in_student": "studentSession",
-    "log_in_teacher": "teacherSession"
+    "log_in_teacher": "teacherSession",
+    "sign_up_student": "registerStudent",
+    "sign_up_teacher": "registerTeacher"
   },
   getAlbum: function(name) {
     App.fetchSongs(name);
@@ -13,6 +15,14 @@ var Router = Backbone.Router.extend({
   teacherSession: function() {
     var person_logging_in = "Teacher";
     App.getLogInForm(person_logging_in);
+  },
+  registerStudent: function() {
+    var person_registering = "Student";
+    App.getRegForm(person_registering);
+  },
+  registerTeacher: function() {
+    var person_registering = "Teacher";
+    App.getRegForm(person_registering);
   },
   index: function() {
     var modal = App.songs || App.log_in_form
