064b2bbd20a73a4eff5d9f70bcec559c52843274
mattdvhope <mattagape@me.com>
2015-11-11 10:00:25 +0700

    Added App.reg_form = undefined

--- app/assets/javascripts/backbone/routers/front.js
+++ app/assets/javascripts/backbone/routers/front.js
@@ -1,19 +1,19 @@
 var FrontRouter = Backbone.Router.extend({
   routes: {
-    "": "openFrontPage",
+    // "": "openFrontPage"
   },
   openFrontPage: function() {
-console.log("in router?????");
-    App.getFrontPage;
+    // App.getFrontPage;
   },
   index: function() {
-    var modal = App.songs || App.log_in_form
+    var modal = App.reg_form || App.log_in_form;
     if (!modal.$el.is(":animated")) { // ':animated' (a jQuery pseudo-selector) here refers to 'faded in' ('fadeIn' is currently in operation)
       modal.fadeOut();
     }
+    App.reg_form = undefined;
   },
   initialize: function() {
-    // this.route(/^\/?$/, "index", this.index); // listening for a path that starts with a '/' which will be our 'index' & we'll call the current 'index' method in 'Router'
+    this.route(/^\/?$/, "index", this.index); // listening for a path that starts with a '/' which will be our 'index' & we'll call the current 'index' method in 'Router'
   }
 });
       
