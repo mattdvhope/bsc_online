885a68e1471418d8ebc5c543460c459cccd2b3ab
mattdvhope <mattagape@me.com>
2015-11-12 01:08:39 +0700

    Consolidated routers into one file

--- app/assets/javascripts/backbone/routers/log_in_reg.js
+++ app/assets/javascripts/backbone/routers/log_in_reg.js
@@ -22,13 +22,14 @@ var LogRegRouter = Backbone.Router.extend({
     App.getRegForm(person_registering);
   },
   index: function() {
-    // var modal = App.reg_form || App.log_in_form
-    // if (!modal.$el.is(":animated")) { // ':animated' (a jQuery pseudo-selector) here refers to 'faded in' ('fadeIn' is currently in operation)
-    //   modal.fadeOut();
-    // }
+    var modal = App.reg_form || App.log_in_form;
+    if (!modal.$el.is(":animated")) { // ':animated' (a jQuery pseudo-selector) here refers to 'faded in' ('fadeIn' is currently in operation)
+      modal.fadeOut();
+    }
+    App.reg_form = undefined;
   },
   initialize: function() {
-    // this.route(/^\/?$/, "index", this.index); // listening for a path that starts with a '/' which will be our 'index' & we'll call the current 'index' method in 'Router'
+    this.route(/^\/?$/, "index", this.index); // listening for a path that starts with a '/' which will be our 'index' & we'll call the current 'index' method in 'FrontRouter'
   }
 });
       
