89552b01b6de26e70fc4ec79319917042f5bf831
mattdvhope <mattagape@me.com>
2015-11-11 11:45:18 +0700

    Removed unneeded methods

--- app/assets/javascripts/backbone/routers/front.js
+++ app/assets/javascripts/backbone/routers/front.js
@@ -1,10 +1,4 @@
 var FrontRouter = Backbone.Router.extend({
-  routes: {
-    // "": "openFrontPage"
-  },
-  openFrontPage: function() {
-    // App.getFrontPage;
-  },
   index: function() {
     var modal = App.reg_form || App.log_in_form;
     if (!modal.$el.is(":animated")) { // ':animated' (a jQuery pseudo-selector) here refers to 'faded in' ('fadeIn' is currently in operation)
