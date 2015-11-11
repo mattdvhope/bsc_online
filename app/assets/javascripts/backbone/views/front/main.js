b0ac1a879454b2efb48a0fc4f60261ce5cd1d6e5
mattdvhope <mattagape@me.com>
2015-11-12 04:36:23 +0700

    Removed logInStudent method

--- app/assets/javascripts/backbone/views/front/main.js
+++ app/assets/javascripts/backbone/views/front/main.js
@@ -2,12 +2,6 @@ var MainFrontView = Backbone.View.extend({
   attributes: {
     // id: "entry_form_modal"
   },
-  events: {
-    "click span a.link-button": "logInStudent"
-  },
-  logInStudent: function() {
-    App.getLogInForm("Student");
-  },
   templateMainFront:  HandlebarsTemplates['front/main'],
   render: function() {
     this.$el.html(this.templateMainFront());
