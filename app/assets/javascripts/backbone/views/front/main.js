8dad11c71c26e71705d06481e3e1b727ace07352
mattdvhope <mattagape@me.com>
2015-11-12 11:16:34 +0700

    Removed unused methods

--- app/assets/javascripts/backbone/views/front/main.js
+++ app/assets/javascripts/backbone/views/front/main.js
@@ -1,7 +1,4 @@
 var MainFrontView = Backbone.View.extend({
-  attributes: {
-    // id: "entry_form_modal"
-  },
   templateMainFront:  HandlebarsTemplates['front/main'],
   render: function() {
     this.$el.html(this.templateMainFront());
