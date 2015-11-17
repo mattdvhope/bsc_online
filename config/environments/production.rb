524fd66c37870cb72e6dd7944d333a62db72a603
mattdvhope <mattagape@me.com>
2015-11-17 17:29:02 +0700

    Had set up smtp, but then did not need it.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,27 +62,6 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  # config.action_mailer.delivery_method = :smtp
-  # SMTP settings for mailgun
-  # ActionMailer::Base.smtp_settings = {
-  #   :port           => 587,
-  #   :address        => "smtp.mailgun.org",
-  #   :domain         => ENV['domain'],
-  #   :user_name      => ENV['username'],
-  #   :password       => ENV['password'],
-  #   :authentication => :plain
-  # }
-
-  config.action_mailer.delivery_method = :mailgun
-  config.action_mailer.mailgun_settings = {
-    api_key: ENV['api_key'],
-    domain: ENV['domain']'
-  }
-
-
-
-
-
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
