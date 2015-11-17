8a1a8c3ddf2e06280995087faa1f21448b025b24
mattdvhope <mattagape@me.com>
2015-11-17 18:48:33 +0700

    Began work on directing users w/ different roles to different paths

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,7 +15,12 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:password]) # The 'authenticate' method is given to us by the Rails 'has_secure_password' in user.rb
       session[:user_id] = user.id
       flash[:success] = "You are logged in #{user.first_name} #{user.last_name}, enjoy!"
-      redirect_to home_path 
+      # redirect_to home_path
+      if user.role_id == 1
+        redirect_to home_path
+      elsif user.role_id == 2
+        redirect_to curriculums_path
+      end
     else
       flash[:danger] = "Invalid email or password."
       redirect_to root_path
