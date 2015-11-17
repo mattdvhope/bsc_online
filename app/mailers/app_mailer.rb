437dc59c885c35589cffbf824b79cd8d9c671424
mattdvhope <mattagape@me.com>
2015-11-17 17:29:40 +0700

    Set up sample_mailer method

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -6,40 +6,14 @@ class AppMailer < ActionMailer::Base
     mail to: user.email, subject: "Welcome to the City English Project (CEP)!"
   end
 
-  # def send_forgot_password(user)
-  #   @user = user
-  #   mail to: user.email, subject: "Please reset your password."
-  # end
-
-  # def send_invitation_email(invitation)
-  #   @invitation = invitation
-  #   mail to: invitation.recipient_email, subject: "Invitation to join MyFlix"
-  # end
-
-  # def send_card_problem_email(user)
-  #   @user = user
-  #   mail to: user.email, subject: "Your MyFlix account has been suspended."
-  # end
-
   def sample_email(user)
     @user = user
-    mail({
-      :from    => ENV['username'],
-      :to      => @user.email,
-      :subject => "Sample Mail using Mailgun API.",
-      :text    => "This mail is sent using Mailgun API via mailgun-ruby."
-    })
+    mg_client = Mailgun::Client.new ENV["api_key"]
+    message_params = {:from    => ENV["username"],
+                      :to      => @user.email,
+                      :subject => 'Sample Mail using Mailgun API',
+                      :text    => 'This mail is sent using Mailgun API via mailgun-ruby'}
+    mg_client.send_message ENV["domain"], message_params
   end
 
-#   def sample_email(user)
-#     @user = user
-#     mg_client = Mailgun::Client.new ENV['api_key']
-#     message_params = {:from    => ENV['username'],
-#                       :to      => @user.email,
-#                       :subject => 'Sample Mail using Mailgun API',
-#                       :text    => 'This mail is sent using Mailgun API via mailgun-ruby'}
-#     mg_client.send_message ENV['domain'], message_params
-#   end
-
-
-# end
+end
