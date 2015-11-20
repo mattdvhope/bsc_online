140a03697958d5f03633527b00ab7744c230b5fc
mattdvhope <mattagape@me.com>
2015-11-20 11:28:38 +0700

    Set up mailer and view called sample_mail

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -1,18 +1,19 @@
 class AppMailer < ActionMailer::Base
-  default from: "mattagape@me.com"
 
   def send_welcome_email(user)
-    @user = user # '@user' available for email which is /view/app_mailer/send_welcome_email.html.haml
-    mail to: user.email, subject: "Welcome to the City English Project (CEP)!"
+    @user = user
+    mail from: "mattagape@me.com", to: user.email, subject: "Welcome to the [Development] City English Project (CEP)!"
   end
 
   def sample_email(user)
     @user = user
     mg_client = Mailgun::Client.new ENV["api_key"]
-    message_params = {:from    => ENV["username"],
-                      :to      => @user.email,
-                      :subject => 'Welcome to CEP!',
-                      :text    => 'We hope you enjoy being a part of CEP!'}
+    message_params = {
+      :from    => ENV["username"],
+      :to      => @user.email,
+      :subject => "Welcome to CEP, #{@user.name}!",
+      :html => (render_to_string(template: "../views/app_mailer/sample_email")).to_str
+    }
     mg_client.send_message ENV["domain"], message_params
   end
 
