42bd11df7a68dabb4903949201d37e50ef97d26f
mattdvhope <mattagape@me.com>
2015-11-26 06:01:35 +0700

    Set up 'checkInputs' method

--- app/assets/javascripts/backbone/views/registration/student_reg.js
+++ app/assets/javascripts/backbone/views/registration/student_reg.js
@@ -5,13 +5,57 @@ var StudentRegFormView = Backbone.View.extend({
     id: "entry_form_modal"
   },
   events: {
-    "click a.close": "close"
+    "click a.close": "close",
+    "click input.student_reg_er": "checkInputs"
   },
   duration: 300,
   templateStudentReg:  HandlebarsTemplates['registration/student_reg'],
   open: function () {
     this.$el.add($overlay).fadeIn(this.duration);
   },
+  checkInputs: function(e) {
+    $(".student_registration").css("border-color", "blue");
+    var someEmpty = $('.student_registration').filter(function(){
+      return $.trim(this.value).length === 0;
+    }).length > 0;
+
+    if (someEmpty) {
+      e.preventDefault();
+      this.highlightEmptyField("#user_first_name", "your first name");
+      this.highlightEmptyField("#user_last_name", "your last name");
+      this.highlightEmptyField("#user_email", "an email address");
+      this.highlightEmptyField("#user_password", "your password");
+      this.highlightEmptyField("#user_password_confirmation", "your password confirmation");
+      this.highlightEmptyField("#user_postal_code", "a postal code");
+    }
+
+    if ($("#user_password").val() !== $("#user_password_confirmation").val()) {
+      e.preventDefault();
+      $("input[type='password']").val("").css("border-color", "red").attr("placeholder", "Your passwords must match.");
+    }
+
+    if (this.verifyEmail($("#user_email").val()) === false) {
+      e.preventDefault();
+      $("input[type='email']").val("").css("border-color", "red").attr("placeholder", "Enter a valid email.");
+    }
+
+    if (this.verifyPostalCode($("#user_postal_code").val()) === false) {
+      e.preventDefault();
+      $("input[id='user_postal_code']").val("").css("border-color", "red").attr("placeholder", "Enter a valid postal code.");
+    }
+
+  },
+  highlightEmptyField: function(input_id, text) {
+    if ($(input_id).val() === "") {
+      $(input_id).css("border-color", "red").attr("placeholder", "You must enter " + text);
+    }
+  },
+  verifyEmail: function(email) {
+    return (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email))
+  },
+  verifyPostalCode: function(post) {
+    return (/^(\d{5})?$/.test(post))
+  }, // FIX this REGEX to correspond to the Ruby REGEX in 'user.rb'!!!
   close: function(e) {
     e.preventDefault();
     this.fadeOut();
@@ -31,6 +75,9 @@ var StudentRegFormView = Backbone.View.extend({
     this.open(); // to fade the overlay in...
   },
   initialize: function() {
+    // $(this.el).html(this.template(this.model.toJSON())); 
+
+
     this.$el.appendTo(document.body);
   }
 });
