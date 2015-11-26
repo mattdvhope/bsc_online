538f197e7b191610533a69a0fd980737ad67cc4a
mattdvhope <mattagape@me.com>
2015-11-26 09:17:47 +0700

    Changed to 'volunteer_applicant' mailer, etc

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -5,14 +5,26 @@ class AppMailer < ActionMailer::Base
     mail from: "mattagape@me.com", to: user.email, subject: "Welcome to the [Development] City English Project (CEP)!"
   end
 
-  def sample_email(user)
+  def volunteer_applicant(user)
     @user = user
     mg_client = Mailgun::Client.new ENV["api_key"]
     message_params = {
       :from    => ENV["username"],
       :to      => @user.email,
       :subject => "Welcome to CEP, #{@user.name}!",
-      :html => (render_to_string(template: "../views/app_mailer/sample_email")).to_str
+      :html => (render_to_string(template: "../views/app_mailer/volunteer_applicant")).to_str
+    }
+    mg_client.send_message ENV["domain"], message_params
+  end
+
+  def student_welcome(user)
+    @user = user
+    mg_client = Mailgun::Client.new ENV["api_key"]
+    message_params = {
+      :from    => ENV["username"],
+      :to      => @user.email,
+      :subject => "Welcome to CEP, #{@user.name}!",
+      :html => (render_to_string(template: "../views/app_mailer/student_welcome")).to_str
     }
     mg_client.send_message ENV["domain"], message_params
   end
