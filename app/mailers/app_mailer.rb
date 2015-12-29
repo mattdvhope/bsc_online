b788699aa02cccdf5d47a41546ed2bf5b036f5eb
mattdvhope <mattagape@me.com>
2015-12-29 16:09:39 +0700

    Made 'pretty' from's

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -2,7 +2,7 @@ class AppMailer < ActionMailer::Base
 
   def development_env_email(user)
     @user = user
-    mail from: ENV["username"], to: user.email, subject: "Welcome to the [Development] City English Project (CEP)!"
+    mail from: "City English Project <" + ENV["username"] + ">", to: user.email, subject: "Welcome to the [Development] City English Project (CEP)!"
   end
 
   def admin_applicant(user)
@@ -11,7 +11,7 @@ class AppMailer < ActionMailer::Base
     @user.save
     mg_client = Mailgun::Client.new ENV["api_key"]
     message_params = {
-      :from    => ENV["username"],
+      :from    => "City English Project <" + ENV["username"] + ">",
       :to      => @user.email,
       :subject => "Welcome to CEP, #{@user.first_name}!",
       :html => (render_to_string(template: "../views/app_mailer/admin_applicant")).to_str
@@ -22,8 +22,10 @@ class AppMailer < ActionMailer::Base
   def student_welcome(user)
     @user = user
     mg_client = Mailgun::Client.new ENV["api_key"]
+binding.pry
     message_params = {
-      :from    => ENV["username"],
+      # :from    => "City English Project <" + ENV["username"] + ">",
+      :from    => "City English Project <" + ENV["username"] + ">",
       :to      => @user.email,
       :subject => "Welcome to CEP, #{@user.first_name}!",
       :html => (render_to_string(template: "../views/app_mailer/student_welcome")).to_str
@@ -35,7 +37,7 @@ class AppMailer < ActionMailer::Base
     @applicant = applicant
     mg_client = Mailgun::Client.new ENV["api_key"]
     message_params = {
-      :from    => ENV["username"],
+      :from    => "City English Project <" + ENV["username"] + ">",
       :to      => @applicant.email,
       :subject => "Copy of CEP Volunteer Administrator Application for #{applicant.first_name} #{applicant.last_name}",
       :html => (render_to_string(template: "../views/app_mailer/send_admin_application_form")).to_str
@@ -47,7 +49,7 @@ class AppMailer < ActionMailer::Base
     @applicant = applicant
     mg_client = Mailgun::Client.new ENV["api_key"]
     message_params = {
-      :from    => ENV["username"],
+      :from    => "City English Project <" + ENV["username"] + ">",
       :to      => ENV["other_username"],
       :subject => "CEP Volunteer Administrator Application from #{applicant.first_name} #{applicant.last_name}",
       :html => (render_to_string(template: "../views/app_mailer/send_cep_application_form")).to_str
@@ -59,7 +61,7 @@ class AppMailer < ActionMailer::Base
     @applicant = applicant
     mg_client = Mailgun::Client.new ENV["api_key"]
     message_params = {
-      :from    => ENV["username"],
+      :from    => "City English Project <" + ENV["username"] + ">",
       :to      => @applicant.email,
       :subject => "Congratulations #{applicant.first_name}! You are approved as a CEP Volunteer Administrator!",
       :html => (render_to_string(template: "../views/app_mailer/send_admin_application_approval")).to_str
