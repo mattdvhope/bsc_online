b63105de6d68314398fdd5454440f28e30b7915e
mattdvhope <mattagape@me.com>
2015-12-29 11:37:51 +0700

    Worked on 'email_admin_application_approval'

--- app/controllers/admin_applications_controller.rb
+++ app/controllers/admin_applications_controller.rb
@@ -7,6 +7,9 @@ class AdminApplicationsController < ApplicationController
   def update
     @admin_application = AdminApplication.find(params[:id])
     if @admin_application.update(admin_application_params)
+      user = @admin_application.user
+      user.update_attribute(:pin, user.generate_pin.to_s)
+      user.save!(:validate => false)
       flash[:success] = "Thank you for sending in this CEP Volunteer Administrator Application!"
       send_application_emails(@admin_application.user)
       redirect_to root_path
@@ -18,14 +21,14 @@ class AdminApplicationsController < ApplicationController
 
   def email_admin_application_approval
     admin_application = AdminApplication.find(params[:id])
-    admin_application.update(approved: true)
+    admin_application.update_attribute(:approved, true)
+    applicant = admin_application.user
 
-
-    # if Rails.env.production?
-    #   AppMailer.send_admin_application_approval(applicant).deliver_later
-    # else
-    #   send_development_email(applicant)
-    # end
+    if Rails.env.production?
+      AppMailer.send_admin_application_approval(applicant).deliver_later
+    else
+      send_development_email(applicant)
+    end
 
     redirect_to :back
   end
@@ -40,7 +43,8 @@ class AdminApplicationsController < ApplicationController
       if Rails.env.production?
         send_production_emails(applicant)
       else
-        send_development_email(applicant)
+        send_production_emails(applicant)
+        # send_development_email(applicant)
       end
     end
 
