447b1917c85afb49443becf2a2106853a69ab2dd
mattdvhope <mattagape@me.com>
2016-02-04 13:29:28 +0700

    Set up fadeOut method

--- app/assets/javascripts/backbone/views/front/main_thai.js
+++ app/assets/javascripts/backbone/views/front/main_thai.js
@@ -1,5 +1,11 @@
 var MainThaiView = Backbone.View.extend({
   templateMainThaiFront:  HandlebarsTemplates['front/main_thai'],
+  fadeOut: function() {
+    this.$el.fadeOut(0, function() {
+      this.remove();
+    }.bind(this));
+    App.getFrontMainPage();
+  },
   render: function() {
     this.$el.html(this.templateMainThaiFront());
   },
