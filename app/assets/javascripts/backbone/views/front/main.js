d4a2e6756c05e1780b2905189f18d2f29fff441b
mattdvhope <mattagape@me.com>
2016-02-05 14:31:05 +0700

    Began setting up functionality for switching between english and thai partials

--- app/assets/javascripts/backbone/views/front/main.js
+++ app/assets/javascripts/backbone/views/front/main.js
@@ -1,11 +1,50 @@
 var MainFrontView = Backbone.View.extend({
-  templateMainFront:  HandlebarsTemplates['front/main'],
+  template:  HandlebarsTemplates['front/main'],
   render: function() {
-    this.$el.html(this.templateMainFront());
+    this.$el.html(this.template({
+      thai_flag_present: this.thai_flag_up(),
+      login: this.login_button_language(),
+      register: this.register_button_language(),
+      volunteer: this.volunteer_button_language(),
+      features: this.features_language(),
+      step_one: this.step_one(),
+      step_two: this.step_two(),
+      helping_friends: this.helping_friends()
+    }));
+  },
+  thai_flag_up: function() {
+    return $(".thai_flag").is(":visible");
+  },
+  login_button_language: function() {
+    return this.switch_languages("Login", "เข้าสู่ระบบ");
+  },
+  register_button_language: function() {
+    return this.switch_languages("Register", "ลงทะเบียน");
+  },
+  volunteer_button_language: function() {
+    return this.switch_languages("Volunteer!", "รับอาสา!");
+  },
+  features_language: function() {
+    return this.switch_languages("Features", "คุณลักษณะเด่น");
+  },
+  step_one: function() {
+    return this.switch_languages("Step One", "ขั้นตอนหนึ่ง");
+  },
+  step_two: function() {
+    return this.switch_languages("Step Two", "ขั้นตอนที่สอง");
+  },
+  helping_friends: function() {
+    return this.switch_languages("Helping Friends", "ช่วยให้เพื่อน ๆ");
+  },
+  switch_languages: function(english, thai) {
+    if (this.thai_flag_up()) {
+      return english;
+    } else {
+      return thai
+    }
   },
   initialize: function() {
     this.$el.appendTo(".entire-main");
   }
 });
-
 
--- app/assets/javascripts/backbone/views/volunteer/volunteer_info.js
+++ app/assets/javascripts/backbone/views/volunteer/volunteer_info.js
@@ -7,7 +7,21 @@ var VolunteerPageView = Backbone.View.extend({
     App.getFrontMainPage();
   },
   render: function() {
-    this.$el.html(this.template());
+    this.$el.html(this.template({
+      thai_flag_present: this.thai_flag_up()
+
+    }));
+  },
+  thai_flag_up: function() {
+    return $(".thai_flag").is(":visible");
+  },
+
+  switch_languages: function(english, thai) {
+    if (this.thai_flag_up()) {
+      return english;
+    } else {
+      return thai
+    }
   },
   initialize: function() {
     this.$el.appendTo(".entire-main");
