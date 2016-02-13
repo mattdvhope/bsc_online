ac94530d1fc0b7548e106978f0bd1f9aee2428b3
mattdvhope <mattagape@me.com>
2016-02-13 16:23:30 +0700

    Edited 'volunteers' method to allow for both a volunteer & and admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,9 @@ class UsersController < ApplicationController
   end
 
   def volunteers
-    @volunteers = User.where(role: "admin")
+    admin_folks = User.where(role: "admin")
+    volunteer_folks = User.where(role: "volunteer")
+    @volunteers = admin_folks.concat(volunteer_folks)
   end
 
   def student_connect_with_volunteer
@@ -39,7 +41,6 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
-    User.pins_available # to ensure the latest PIN works; a pins_available 'refresh'!
     log_out_path if users_path
     set_user_session(user)
     if user.valid?
@@ -117,13 +118,16 @@ class UsersController < ApplicationController
       if Rails.env.production?
         send_production_email(user)
       else
-        send_development_email(user)
+        send_production_email(user)
+        # send_development_email(user)
       end
     end
 
     def send_production_email(user)
       if user.role == "admin_applicant"
         AppMailer.admin_applicant(user).deliver_later
+      elsif user.role == "volunteer"
+        AppMailer.volunteer_welcome(user).deliver_later
       else
         AppMailer.student_welcome(user).deliver_later
       end
@@ -133,7 +137,8 @@ class UsersController < ApplicationController
       if Rails.env.production?
         AppMailer.student_to_volunteer(student, volunteer).deliver_later
       else
-        send_development_email(student)
+        AppMailer.student_to_volunteer(student, volunteer).deliver_later
+        # send_development_email(student)
       end
     end
 
