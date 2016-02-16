087b25a41c4e7be397701e590df4be9a7eeff483
mattdvhope <mattagape@me.com>
2016-02-16 13:34:22 +0700

    Extracted code to here from app.js

---
+++ app/assets/javascripts/browser_storage.js
@@ -0,0 +1,37 @@
+// Deal with browsers that don't allow session or local storage...
+
+try {
+  // Initialize sessionStorage key-value pairs//////////
+  if (sessionStorage.getItem("fragment") === null) {
+    sessionStorage.setItem("fragment", "");
+  }
+  if (sessionStorage.getItem("language") === null) {
+    sessionStorage.setItem("language", "english");
+  }
+  //////////////////////////////////////////////////////
+}
+catch (e) {
+  // alert("Please turn off 'private' for this browser to make this app work much faster.");
+  swal({
+    title: "Please turn off 'private' for this browser or use Chrome.",
+    text: "It will run slowly in 'private.'",
+    timer: 15000,
+    showConfirmButton: true,
+    animation: "slide-from-bottom"
+  });
+}
+
+
+function sessionStorageAvailable(key) {
+  return sessionStorage.getItem(key) !== null
+}
+
+if (sessionStorageAvailable("fragment")) {
+  $("#home-link").on("click", function() {
+    sessionStorage.setItem('fragment', "");
+  });
+  $(".login-checker").on("click", function() {
+    sessionStorage.setItem('fragment', "");
+  });
+}
+
