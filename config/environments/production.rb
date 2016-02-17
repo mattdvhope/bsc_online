08c834078bcfd0cd971f7e1a22d6a05f639b68f5
mattdvhope <mattagape@me.com>
2016-02-17 12:23:39 +0700

    Reworded comment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
-  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
+  # Set up CDN for serving up assets.
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Ignore bad email addresses and do not raise email delivery errors.
