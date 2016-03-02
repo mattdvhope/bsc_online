15469d24d4a54151d0731380b5d168cb8da668bf
mattdvhope <mattagape@me.com>
2016-03-02 12:05:21 +0700

    Changed page to volunteer_page in index method

--- app/assets/javascripts/backbone/routers/router.js
+++ app/assets/javascripts/backbone/routers/router.js
@@ -35,16 +35,16 @@ var Router = Backbone.Router.extend({
   },
   index: function() {
     var modal = App.reg_form || App.log_in_form;
-    var page = App.volunteer_page;
-    if (modal && page) {
+    var volunteer_page = App.volunteer_page;
+    if (modal && volunteer_page) {
       App.getFrontMainPage();
     } else if (modal) {
       if (modal.$el.is(":visible")) {
         modal.fadeOut();
       }
-    } else if (page) {
-      if (page.$el.is(":visible")) {
-        page.fadeOut();
+    } else if (volunteer_page) {
+      if (volunteer_page.$el.is(":visible")) {
+        volunteer_page.fadeOut();
       }
     }
     App.reg_form = undefined;
