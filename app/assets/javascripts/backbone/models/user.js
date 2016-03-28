cb76808c55108f92395a8ce49a30028612ea60c6
mattdvhope <mattagape@me.com>
2016-03-28 13:38:35 +0700

    Experimented w/ user model

--- app/assets/javascripts/backbone/models/user.js
+++ app/assets/javascripts/backbone/models/user.js
@@ -6,6 +6,7 @@ var User = Backbone.Model.extend({
   url : function() {
     var base = 'users';
     if (this.isNew()) return base;
-    return base + (base.charAt(base.length - 1) == '/' ? '' : '/') + this.id;
-  }
+    return base + (base.charAt(base.length - 1) == '/' ? '' : '/') + this.id + '.json';
+  },
+  urlRoot: 'users'
 });
