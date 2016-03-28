8ac0d0f5bcd8ce7442ee9e0738b306e6dc9b3b06
mattdvhope <mattagape@me.com>
2016-03-28 13:39:38 +0700

    Commented out scroll top

--- app/assets/javascripts/nav_links.js
+++ app/assets/javascripts/nav_links.js
@@ -1,3 +1,22 @@
 // Change out nav link list when changing pages
 
-$($("div.navbar-collapse").parent().parent().children().last().find( "ul" )).hide();
+// $($("a[href$='volunteer_info']").parent().parent().children().last().find( "ul" )).hide();
+
+$(document).on("click tap", "a[href$='volunteer_info']", function() {
+
+  $($("div.navbar-collapse>").find("a[href$='#features']")).hide()
+  $($("div.navbar-collapse>").find("a[href$='#process']")).hide()
+  $($("div.navbar-collapse>").find("a[href$='#world-view']")).hide()
+  $($("div.navbar-collapse>").find("a[href$='#helping-friends']")).hide()
+
+    // if (sessionStorageAvailable("fragment")) {
+    //   sessionStorage.setItem('language', "thai");    
+    // }
+    // var tempScrollTop = $("body").scrollTop();
+    // $($(this).parent().find( ".thai_flag" )).hide();
+    // $($(this).parent().find( ".usa_flag" )).show();
+    // getCorrectPageWhenFlagClicked();
+    // $("body").scrollTop(tempScrollTop);
+
+});
+
