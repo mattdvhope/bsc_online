54855dde73b3bed01d71014589569f0b9580e3ba
mattdvhope <mattagape@me.com>
2016-04-06 10:15:47 +0700

    Started work on moving dashboard to Backbone

---
+++ app/assets/javascripts/backbone/views/dashboard/dashboard.js
@@ -0,0 +1,19 @@
+var DashboardView = Backbone.View.extend({
+  template:  HandlebarsTemplates['dashboard/dashboard'],
+  // fadeOut: function() {
+  //   this.$el.fadeOut(0, function() {
+  //     this.remove();
+  //   }.bind(this));
+  //   App.getFrontMainPage();
+  // },
+  render: function() {
+console.log("in render?");
+    this.$el.html(this.template());
+  },
+  initialize: function() {
+    this.listenTo(this.model, 'sync', this.render());
+    this.$el.appendTo(".entire-main");
+  }
+});
+
+
