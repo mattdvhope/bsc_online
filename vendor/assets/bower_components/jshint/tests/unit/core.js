d5e656a33d889499d79d8e8ff9dfee10ff3d0687
mattdvhope <mattagape@me.com>
2016-05-09 06:11:31 +0700

    Cleaned out Bower

--- vendor/assets/bower_components/jshint/tests/unit/core.js
+++ vendor/assets/bower_components/jshint/tests/unit/core.js
@@ -98,11 +98,15 @@ exports.testExportedDefinedGlobals = function (test) {
 exports.testGlobalVarDeclarations = function (test) {
   var src = "var a;";
 
-  // Test should pass
-  TestRun(test).test(src, { es3: true, node: true }, {});
+  TestRun(test).test(src, { es3: true }, {});
 
   var report = JSHINT.data();
   test.deepEqual(report.globals, ['a']);
+
+  TestRun(test).test(src, { es3: true, node: true }, {});
+
+  report = JSHINT.data();
+  test.strictEqual(report.globals, undefined);
 
   TestRun(test).test("var __proto__;", { proto: true });
   report = JSHINT.data();
@@ -2028,6 +2032,60 @@ exports["gh-2761"] = function (test) {
     .addError(6, "Reassignment of 'a', which is is a function. " +
               "Use 'var' or 'let' to declare bindings that may change.")
     .test(code);
+
+  test.done();
+};
+
+exports["gh-2838"] = function (test) {
+
+  var code = [
+    "function foo() {",
+    "  return a + b;",
+    "}",
+    "function bar() {",
+    "  return a + b;",
+    "}",
+    "let a = 1;",
+    "const b = 2;"
+  ];
+
+  TestRun(test).test(code, { esversion: 6 });
+
+  code = [
+    "function x() {",
+    "  return c;",
+    "}",
+    "void c;",
+    "let c;"
+  ];
+
+  TestRun(test, "Same-scope reference following sub-scope reference")
+    .addError(5, "'c' was used before it was declared, which is illegal for 'let' variables.")
+    .test(code, { esversion: 6 });
+
+  code = [
+    "function x() {",
+    "  return d;",
+    "}",
+    "({ d } = {});",
+    "let d;"
+  ];
+
+  TestRun(test, "Same-scope assignment following sub-scope reference")
+    .addError(5, "'d' was used before it was declared, which is illegal for 'let' variables.")
+    .test(code, { esversion: 6 });
+
+  test.done();
+};
+
+exports["destructuring in setter parameter"] = function (test) {
+
+  TestRun(test).test([
+    "var a = {",
+    "  get x() {},",
+    "  set x({ a, b }) {}",
+    "};"
+  ], { esversion: 6 });
 
   test.done();
 };
