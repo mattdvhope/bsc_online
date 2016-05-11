6e3b18f213dd3c766fd95f4f2a5c061d81a1fd7d
mattdvhope <mattagape@me.com>
2016-05-11 13:58:05 +0700

    Commented out content of this file

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,18 +1,18 @@
 # in AWS, go to IAM > Users > Permissions > Attach Policy > [scroll way down to...] AmazonS3FullAccess
 
-CarrierWave.configure do |config|
-  if Rails.env.production?
-    config.fog_credentials = {
-      :provider               => 'AWS',
-      :aws_access_key_id      => ENV['S3_KEY'],
-      :aws_secret_access_key  => ENV['S3_SECRET'],
-      :region                 => ENV['S3_REGION']
-    }
-    config.fog_directory  = ENV['S3_BUCKET']
-  else
-    config.storage = :file
-    config.enable_processing = Rails.env.development? # If we're only running tests, we'll not use mini_magick to process the images.
-  end
-end
+# CarrierWave.configure do |config|
+#   if Rails.env.production?
+#     config.fog_credentials = {
+#       :provider               => 'AWS',
+#       :aws_access_key_id      => ENV['S3_KEY'],
+#       :aws_secret_access_key  => ENV['S3_SECRET'],
+#       :region                 => ENV['S3_REGION']
+#     }
+#     config.fog_directory  = ENV['S3_BUCKET']
+#   else
+#     config.storage = :file
+#     config.enable_processing = Rails.env.development? # If we're only running tests, we'll not use mini_magick to process the images.
+#   end
+# end
 
 
