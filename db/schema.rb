cbbd9fb023a9613b6b0f5096ce369e6da34a9d0b
mattdvhope <mattagape@me.com>
2016-05-12 04:43:44 +0700

    Created photo model and table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160511204548) do
+ActiveRecord::Schema.define(version: 20160511214148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -114,6 +114,12 @@ ActiveRecord::Schema.define(version: 20160511204548) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.string   "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "plans", force: :cascade do |t|
