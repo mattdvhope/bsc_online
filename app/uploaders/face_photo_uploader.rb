30a85d91ac2af2ea18c6f69dbb2cb59a358283c5
mattdvhope <mattagape@me.com>
2016-05-11 10:20:04 +0700

    Set up cache_dir

--- app/uploaders/face_photo_uploader.rb
+++ app/uploaders/face_photo_uploader.rb
@@ -1,4 +1,3 @@
-
 class FacePhotoUploader < CarrierWave::Uploader::Base
 
   include CarrierWave::MiniMagick
@@ -6,6 +5,10 @@ class FacePhotoUploader < CarrierWave::Uploader::Base
   process :resize_to_fill => [156, 200] #The '#enable_processing' method in 'carrier_wave.rb' allows for the usage of this 'process' (for production? too??)
 
   storage :fog
+
+  def cache_dir
+    "#{Rails.root}/tmp/uploads"
+  end
 
   def extension_white_list
     %w(jpg jpeg gif png)
