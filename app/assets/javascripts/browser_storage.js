a9561c6d691fcaabd36a95e46cdd66c32818d116
mattdvhope <mattagape@me.com>
2016-05-16 02:25:22 +0700

    Changed default language to thai

--- app/assets/javascripts/browser_storage.js
+++ app/assets/javascripts/browser_storage.js
@@ -6,7 +6,7 @@ try {
     sessionStorage.setItem("fragment", "");
   }
   if (sessionStorage.getItem("language") === null) {
-    sessionStorage.setItem("language", "english");
+    sessionStorage.setItem("language", "thai");
   }
   //////////////////////////////////////////////////////
 }
