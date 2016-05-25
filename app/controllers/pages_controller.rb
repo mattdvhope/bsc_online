c6ca0cf1d609f87dd022c94fcaa2fa0061d616f1
mattdvhope <mattagape@me.com>
2016-05-25 12:52:49 +0700

    Added methods for new templates

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,7 @@ class PagesController < ApplicationController
     @curriculums = Curriculum.all
 
     if current_user && current_user.role == "leader"
-      redirect_to leader_page_path
+      redirect_to leader_path
     elsif current_user
       redirect_to home_path
     end
@@ -18,16 +18,34 @@ class PagesController < ApplicationController
   end
 
   def dashboard
-    gon.student_id = current_user.id
-    if current_user.role == "admin_applicant"
+    case current_user.role
+    when "admin_applicant"
       redirect_to log_out_path
+    when "leader"
+      redirect_to leader_path
+    when "admin"
+      redirect_to admin_path
+    when "volunteer"
+      redirect_to volunteer_path
+    when "student"
+      redirect_to student_path
     end
-    @volunteers = User.where("users.role = ? OR users.role = ? OR users.role = ?", "leader", "admin", "volunteer")
 
+    gon.student_id = current_user.id
+    @volunteers = User.where("users.role = ? OR users.role = ? OR users.role = ?", "leader", "admin", "volunteer")
   end
 
-  def leader_page
+  def leader
     @admin_applications = AdminApplication.all.order("id ASC")
+  end
+
+  def admin
+  end
+
+  def volunteer
+  end
+
+  def student
   end
 
   def contact
