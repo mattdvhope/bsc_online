391f028a87b538e8db020c9f480cbff6afb04715
mattdvhope <mattagape@me.com>
2016-05-27 17:59:16 +0700

    Commmented out parse

--- app/assets/javascripts/backbone/models/user.js
+++ app/assets/javascripts/backbone/models/user.js
@@ -1,9 +1,9 @@
 var User = Backbone.Model.extend({
 
-  parse: function(attrs) {
-    attrs.user_url = "/users/" + attrs.email;
-    return attrs;
-  },
+  // parse: function(attrs) {
+  //   attrs.user_url = "/users/" + attrs.email;
+  //   return attrs;
+  // },
 
   urlRoot: 'users'
 
