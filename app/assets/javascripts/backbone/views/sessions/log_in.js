5862a81ab5ffdb3762511bbe44f19461bf8be5e7
mattdvhope <mattagape@me.com>
2016-05-30 16:00:29 +0700

    Started setting up bootstrap modal for login

--- app/assets/javascripts/backbone/views/sessions/log_in.js
+++ app/assets/javascripts/backbone/views/sessions/log_in.js
@@ -1,65 +1,63 @@
-var $overlay = $("#overlay");
-
 var LogInFormView = Backbone.View.extend({
   attributes: {
-    id: "entry_form_modal"
+    id: "login-modal"
   },
+
+  initialize: function() {},
+
   events: {
-    "click a.close": "close",
-    "click input.login-checker": "checkInputs"
+    // "click input.login-checker": "checkInputs",
+    'click .login-submit': function (e) {
+      e.preventDefault();
+      this.submit();
+    }
   },
+
+  submit: function() {
+
+
+
+  },
+
   duration: 300,
   templateSession:  HandlebarsTemplates['sessions/log_in'],
-  open: function () {
-    this.$el.add($overlay).fadeIn(this.duration);
-    $("#login_email").focus();
-  },
-  checkInputs: function(e) {
-    $(".user_login").css("border-color", "blue");
-    var someEmpty = $('.user_login').filter(function(){
-      return $.trim(this.value).length === 0;
-    }).length > 0;
+  // checkInputs: function(e) {
+  //   $(".user_login").css("border-color", "blue");
+  //   var someEmpty = $('.user_login').filter(function(){
+  //     return $.trim(this.value).length === 0;
+  //   }).length > 0;
 
-    if (someEmpty) {
-      e.preventDefault();
-      this.highlightEmptyField("#login_email", "an email address");
-      this.highlightEmptyField("#login_password", "your password");
-    }
+  //   if (someEmpty) {
+  //     e.preventDefault();
+  //     this.highlightEmptyField("#login_email", "an email address");
+  //     this.highlightEmptyField("#login_password", "your password");
+  //   }
 
-    if (this.verifyEmail($("#login_email").val()) === false) {
-      e.preventDefault();
-      $("input[type='email']").val("").css("border-color", "red").attr("placeholder", "Enter a valid email.");
-    }
-  },
-  highlightEmptyField: function(input_id, text) {
-    if ($(input_id).val() === "") {
-      $(input_id).css("border-color", "red").attr("placeholder", "You must enter " + text);
-    }
-  },
-  verifyEmail: function(email) {
-    return (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email))
-  },
-  close: function(e) {
-    e.preventDefault();
-    this.fadeOut();
-    history.back();
-  },
-  fadeOut: function() {
-    $overlay.fadeOut(this.duration);
-    this.$el.fadeOut(this.duration, function() {
-      this.remove();
-    }.bind(this));
-    App.log_in_form = undefined;
-  },
-  render: function(person) {
+  //   if (this.verifyEmail($("#login_email").val()) === false) {
+  //     e.preventDefault();
+  //     $("input[type='email']").val("").css("border-color", "red").attr("placeholder", "Enter a valid email.");
+  //   }
+  // },
+  // highlightEmptyField: function(input_id, text) {
+  //   if ($(input_id).val() === "") {
+  //     $(input_id).css("border-color", "red").attr("placeholder", "You must enter " + text);
+  //   }
+  // },
+  // verifyEmail: function(email) {
+  //   return (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email))
+  // },
+  // close: function(e) {
+  //   e.preventDefault();
+  //   this.fadeOut();
+  //   history.back();
+  // },
+  render: function() {
     var csrf_token = $('meta[name=csrf-token]').attr('content');
     this.$el.html(this.templateSession({
       token: csrf_token
     }));
-    this.open(); // to fade the overlay in...
-  },
-  initialize: function() {
-    this.$el.appendTo(document.body);
+
+    return this;
   }
 });
 
