41bd4abf8c08d8b2d98b3f11ea2104120d08b1f8
mattdvhope <mattagape@me.com>
2016-06-06 15:59:04 +0700

    Continuing setting up leader and admin pages to work in Backbone rather than rails

--- app/assets/javascripts/backbone/views/dashboard/guest_students.js
+++ app/assets/javascripts/backbone/views/dashboard/guest_students.js
@@ -2,7 +2,7 @@ var GuestStudentsView = Backbone.View.extend({
   template:  HandlebarsTemplates['dashboard/guest_students'],
 
   initialize: function() {
-    this.listenTo(this.collection, 'all', this.render);
+    // this.listenTo(this.collection, 'all', this.render);
     this.$el.appendTo(".entire");
   },
 
@@ -17,7 +17,6 @@ var GuestStudentsView = Backbone.View.extend({
   },
 
   render: function() {
-console.log(App.guest_students);
     this.$el.html(this.template({
       no_students: this.no_students(),
       students: App.guest_students.toJSON()
