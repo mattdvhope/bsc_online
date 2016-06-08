91e2a2e23c82824eedd0d1c11a526aa9f34ac8b9
mattdvhope <mattagape@me.com>
2016-06-08 14:33:41 +0700

    Attempting prevent scrolling to top on page refresh

--- app/assets/javascripts/transitions.js
+++ app/assets/javascripts/transitions.js
@@ -121,7 +121,7 @@ function showButton(element, index) {
 
 
 // Maintain scroll position when flag clicked; see nav_bar.hbs, 
-$(window).on('scroll', function () {
+$(document).on('scroll', function () {
   var num = $(window).scrollTop();
   sessionStorage.setItem("scrollTopPos", num);
 });
