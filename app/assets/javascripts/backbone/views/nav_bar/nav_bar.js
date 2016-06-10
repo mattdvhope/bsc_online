98025a49115a33c8929eb5affad105d72a4c7a3b
mattdvhope <mattagape@me.com>
2016-06-10 14:50:31 +0700

    Text edits

--- app/assets/javascripts/backbone/views/nav_bar/nav_bar.js
+++ app/assets/javascripts/backbone/views/nav_bar/nav_bar.js
@@ -33,7 +33,7 @@ var NavBarView = Backbone.View.extend({
     return this.choose_language("Student", "ผู้เรียน");
   },
   register_new_student: function() {
-    return this.choose_language("Become Network Member", "กลายเป็นเครือข่ายสมาชิก");
+    return this.choose_language("Become Network Member", "สมัครเป็นสมาชิกเครือข่าย");
   },
   login_student: function() {
     return this.choose_language("Network Member Login", "เครือข่ายสมาชิกเข้าสู่ระบบ");
