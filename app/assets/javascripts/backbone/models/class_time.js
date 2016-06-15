36b3f692c777519b0a20d5add1f849dfb05bb9df
mattdvhope <mattagape@me.com>
2016-06-15 11:49:23 +0700

    Added class times in backbone

---
+++ app/assets/javascripts/backbone/models/class_time.js
@@ -0,0 +1,10 @@
+var ClassTime = Backbone.Model.extend({
+
+  // parse: function(attrs) {
+  //   attrs.user_url = "/users/" + attrs.email;
+  //   return attrs;
+  // },
+
+  urlRoot: 'class_times'
+
+});
