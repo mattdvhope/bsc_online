f35e6680ad126d12ccf94198b78e2e72821fc7c5
mattdvhope <mattagape@me.com>
2016-06-16 17:14:42 +0700

    Set up new and create action pack

--- app/controllers/class_times_controller.rb
+++ app/controllers/class_times_controller.rb
@@ -8,4 +8,25 @@ class ClassTimesController < ApplicationController
     @class_time = ClassTime.find(params[:id])
   end
 
+  def new
+    @class_time = ClassTime.new
+  end
+
+  def create
+    class_time = ClassTime.new(class_time_params)
+    if class_time.save
+      flash[:success] = "You have created a new class time."
+      redirect_to dashboard_path
+    else
+      flash[:danger] = "You were not able to create a new class time. Please ask for help from the CEP web app developer."
+      redirect_to dashboard_path
+    end
+  end
+
+  private
+
+  def class_time_params
+    params.require(:class_time).permit(:period)
+  end
+
 end
