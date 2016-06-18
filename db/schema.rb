44b654e499ff5a27e51bdbd499ce3c961dcdae37
mattdvhope <mattagape@me.com>
2016-06-18 10:12:33 +0700

    Added order_no field

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160617074953) do
+ActiveRecord::Schema.define(version: 20160618031051) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20160617074953) do
     t.datetime "updated_at",  null: false
     t.string   "period_thai"
     t.string   "category"
+    t.integer  "order_no"
   end
 
   create_table "conversations", force: :cascade do |t|
