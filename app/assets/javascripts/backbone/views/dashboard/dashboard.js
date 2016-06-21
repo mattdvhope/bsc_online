765a1e4faceb1bd6dca044e21111339c0924a1af
mattdvhope <mattagape@me.com>
2016-06-21 13:18:55 +0700

    Tweaks on the login Promise

--- app/assets/javascripts/backbone/views/dashboard/dashboard.js
+++ app/assets/javascripts/backbone/views/dashboard/dashboard.js
@@ -6,18 +6,15 @@ var DashboardView = Backbone.View.extend({
   template:  HandlebarsTemplates['dashboard/dashboard'],
 
   initialize: function(options) {
-    this.user_object = options
+    this.options = options
     // this.listenTo(this.model, 'sync', this.render);
     this.$el.appendTo(".entire");
   },
 
   render: function() {
-console.log(this.user_object);
     this.$el.html(this.template({
-      // first_name: this.model.get("first_name"),
-      // role: this.model.get("role"),
-      first_name: this.user_object.user_object.first_name,
-      role: this.user_object.user_object.role
+      first_name: this.options.user_object.first_name,
+      role: this.options.user_object.role
     }));
   }
 });
