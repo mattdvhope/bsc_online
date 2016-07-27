0def9feb225a4e12423fc3b4d2368fd196fc3fe0
mattdvhope <mattagape@me.com>
2016-07-27 10:55:36 +0700

    Removed comments

--- app/assets/javascripts/backbone/views/dashboard/volunteer_profile.js
+++ app/assets/javascripts/backbone/views/dashboard/volunteer_profile.js
@@ -6,39 +6,11 @@ var VolunteerProfileView = Backbone.View.extend({
   template:  HandlebarsTemplates['dashboard/volunteer_profile'],
 
   render: function() {
-    var csrf_token = $('meta[name=csrf-token]').attr('content');
-console.log(this.model.toJSON());
-    // var gender = volunteer.gender
-    // var pronoun;
-    // if (gender === "male") {
-    //   pronoun = "him";
-    //   gender = "man";
-    // } else if (gender === "female") {
-    //   pronoun = "her";
-    //   gender = "woman";
-    // } else if (gender === "ผู้ชาย") {
-    //   pronoun = "him";
-    //   gender = "man";
-    // } else if (gender === "ผู้หญิง") {
-    //   pronoun = "her";
-    //   gender = "woman";
-    // }
     this.$el.html(this.template({
-      // token: csrf_token,
       first_name: this.model.toJSON().first_name,
       last_name: this.model.toJSON().last_name,
-      // gender: gender,
-      // age: volunteer.age,
-      // pronoun: pronoun,
-      // volunteer: volunteer,
-      // student: student
     }));
 
     return this;
   }
 });
-
-
-
-
-
