a5595cb1c859e1bd9dc1001a0e9dd817fcd644c4
mattdvhope <mattagape@me.com>
2016-08-15 13:44:25 +0700

    Changed user attributes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160621084524) do
+ActiveRecord::Schema.define(version: 20160815064051) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -205,11 +205,11 @@ ActiveRecord::Schema.define(version: 20160621084524) do
     t.string   "uid_facebook"
     t.string   "nickname"
     t.string   "image"
-    t.string   "payment_option"
     t.integer  "class_time_id"
     t.string   "class_period"
     t.string   "date_format"
     t.string   "organization"
+    t.string   "national_id"
   end
 
 end
