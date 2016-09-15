55c3f497d3631981efa83901e482035c2f3ac454
mattdvhope <mattagape@me.com>
2016-09-15 12:59:17 +0700

    Commented out index & initialize

--- app/assets/javascripts/backbone/routers/router.js
+++ app/assets/javascripts/backbone/routers/router.js
@@ -22,28 +22,28 @@ var Router = Backbone.Router.extend({
       App.getVolunteerPage();      
     }
   },
-  index: function() {
-    var modal = App.reg_form || App.log_in_form;
-    var volunteer_page = App.volunteer_page;
-    if (modal && volunteer_page) {
-      App.getFrontMainPage();
-    } else if (modal) {
-      if (modal.$el.is(":visible")) {
-        modal.fadeOut();
-      }
-    } else if (volunteer_page) {
-      if (volunteer_page.$el.is(":visible")) {
-        volunteer_page.fadeOut();
-      }
-    }
-    App.reg_form = undefined;
-    App.log_in_form = undefined;
-    App.volunteer_page = undefined;
-  },
-  initialize: function() {
-    this.route(/^\/?$/, "index", this.index); // listening for a path that starts with a '/' which will be our 'index' & we'll call the current 'index' method in 'Router'
-    // this.route(/^\/?volunteer_info$/, "volunteer_info", this.showVolunteerPage);
-  }
+  // index: function() {
+  //   var modal = App.reg_form || App.log_in_form;
+  //   var volunteer_page = App.volunteer_page;
+  //   if (modal && volunteer_page) {
+  //     App.getFrontMainPage();
+  //   } else if (modal) {
+  //     if (modal.$el.is(":visible")) {
+  //       modal.fadeOut();
+  //     }
+  //   } else if (volunteer_page) {
+  //     if (volunteer_page.$el.is(":visible")) {
+  //       volunteer_page.fadeOut();
+  //     }
+  //   }
+  //   App.reg_form = undefined;
+  //   App.log_in_form = undefined;
+  //   App.volunteer_page = undefined;
+  // },
+  // initialize: function() {
+  //   this.route(/^\/?$/, "index", this.index); // listening for a path that starts with a '/' which will be our 'index' & we'll call the current 'index' method in 'Router'
+  //   // this.route(/^\/?volunteer_info$/, "volunteer_info", this.showVolunteerPage);
+  // }
 });
       
 
