322b07ceebb6646e22bdd78f45c02f54296f2b0f
mattdvhope <mattagape@me.com>
2016-09-29 14:38:36 +0700

    Set up student dashboard for multi-language

--- app/assets/javascripts/backbone/views/dashboard/student_dashboard.js
+++ app/assets/javascripts/backbone/views/dashboard/student_dashboard.js
@@ -1,20 +1,40 @@
 var StudentDashboardView = Backbone.View.extend({
+
   attributes: {
     id: "student-dashboard"
   },
-
-  template:  HandlebarsTemplates['dashboard/student_dashboard'],
 
   initialize: function() {
     this.listenTo(this.model, 'sync', this.render);
     this.$el.appendTo(".entire");
   },
 
+  template:  HandlebarsTemplates['dashboard/student_dashboard'],
+
+  welcome_note: function() {
+    return this.choose_language("Welcome to the \"City English Project,\" " + this.model.get("first_name"), "ยินดีต้อนรับสู่ \"โครงการซิตี้ อิงลิช\" " + this.model.get("first_name"));
+
+  },
+
+  app_language_is_thai: function() {
+    return sessionStorage.getItem('language') === "thai";
+  },
+
+  choose_language: function(english, thai) {
+    if (this.app_language_is_thai()) {
+      return thai
+    } else {
+      return english;
+    }
+  },
+
   render: function() {
     this.$el.html(this.template({
-      first_name: this.model.get("first_name"),
+      welcome_note: this.welcome_note(),
+      thai_language: this.app_language_is_thai()
     }));
   }
+
 });
 
 
