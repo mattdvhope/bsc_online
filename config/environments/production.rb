39baf078216149fcc034fd9a905455cabad38c6f
mattdvhope <mattagape@me.com>
2016-11-23 09:55:58 +0700

    Uncommented cloudfront

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Set up CDN for serving up assets.
-  # config.action_controller.asset_host = "d2dfl1mmokbl0b.cloudfront.net"
+  config.action_controller.asset_host = "d2dfl1mmokbl0b.cloudfront.net"
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
