26b8017f2a82e927ec83f73777944a1578a7a3ff
mattdvhope <mattagape@me.com>
2016-11-25 10:14:58 +0700

    Test edit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
-  config.assets.compile = false
+  config.assets.compile = true
 
 
   # Generate digests for assets URLs.
