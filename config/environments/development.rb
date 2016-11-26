162b24dc4105c800f39e70baf3b8de5b9777974f
mattdvhope <mattagape@me.com>
2016-11-26 20:06:36 +0700

    Removed cloudfront

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,6 +38,4 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.action_controller.asset_host = "d2dfl1mmokbl0b.cloudfront.net"
-
 end
