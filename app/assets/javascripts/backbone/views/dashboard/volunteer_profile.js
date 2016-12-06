1dea59f01cfcc1de6fd6213adbd703d83276e4d6
mattdvhope <mattagape@me.com>
2016-12-07 06:50:27 +0700

    Worked on student connecting with volunteer

--- app/assets/javascripts/backbone/views/dashboard/volunteer_profile.js
+++ app/assets/javascripts/backbone/views/dashboard/volunteer_profile.js
@@ -3,6 +3,36 @@ var VolunteerProfileView = Backbone.View.extend({
     id: "student-profile"
   },
 
+  events: {
+    'click #connect-with-volunteer': function (e) {
+      $('#volunteerprofile').modal('hide');
+      swal({
+        title: "สวัสดีครับ -- Thank you for connecting with the CEP Skype teacher!",
+        text: "The Skype teacher will receive an email from CEP with your name and email on it, and will contact you soon.",
+        timer: 20000,
+        showConfirmButton: true,
+        animation: "slide-from-top"
+      });
+      var volunteer_id = $(e.target)[0].dataset.id;
+console.log(volunteer_id);
+      var volunteer = new User({id: volunteer_id});
+      volunteer.fetch({
+        success: function (model, response, options) {
+          console.log("success");
+          console.log(model);
+          // $("entire-main").html(model.get("first_name"));
+
+        },
+        error: function (model, response, options) {
+          console.log("error");
+          console.log(response);
+        }
+      });
+
+
+    }
+  },
+
   template:  HandlebarsTemplates['dashboard/volunteer_profile'],
 
   render: function() {
