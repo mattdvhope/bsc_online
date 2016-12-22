b90979098939aef6fb7c2d83bdf1cf611db64667
mattdvhope <mattagape@me.com>
2016-12-22 19:44:40 +0700

    Set up 'history' modal

--- app/assets/javascripts/backbone/app_methods/get_front_main_page.js
+++ app/assets/javascripts/backbone/app_methods/get_front_main_page.js
@@ -17,6 +17,9 @@ function getFrontMainPage() {
     var cls_dtls_view = new ClassDetailsView();
     $("#classdetailsmodal").html(cls_dtls_view.render().el);
 
+    var history_view = new HistoryView();
+    $("#historymodal").html(history_view.render().el);
+
     App.allowForNestedModals();
 
     App.front_page_main = front_page_main;
