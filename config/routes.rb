fe57a3e98995f5580ee2a01bbc59f0fd2f8e24b3
mattdvhope <mattagape@me.com>
2017-01-23 22:09:00 +0700

    Added update to skype_time_slots

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ Rails.application.routes.draw do
 
   resources :volunteers_available, only: [:index]
 
-  resources :skype_time_slots, only: [:index, :show, :create, :destroy]
+  resources :skype_time_slots, only: [:index, :show, :create, :update, :destroy]
 
   resources :students_needing_skype, only: [:index, :show]
 
