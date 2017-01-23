80acff534d66cfc86d12f57ee212321c8176dfdd
mattdvhope <mattagape@me.com>
2017-01-23 16:43:05 +0700

    Set up biz logic for choosing volunteers available

--- app/controllers/volunteers_available_controller.rb
+++ app/controllers/volunteers_available_controller.rb
@@ -3,11 +3,24 @@ class VolunteersAvailableController < ApplicationController
   before_action :require_user
 
   def index
-    @volunteers = User.where("users.role = ? OR users.role = ?", "admin", "volunteer")
-  end
+    volunteers = User.where("users.role = ? OR users.role = ?", "admin", "volunteer")
 
-  def show
-    @volunteer = User.find(params[:id])
+    vols_with_any_slots = []
+    volunteers.each do |vol|
+      if vol.skype_time_slots.any? 
+        vols_with_any_slots.push(vol)
+      end
+    end
+
+    avail_vols = []
+    vols_with_any_slots.each do |vol|
+      vol.skype_time_slots.each do |slot|
+        avail_vols.push(vol) if slot.available == true
+      end
+    end
+
+    @volunteers = avail_vols
+
   end
 
 end
