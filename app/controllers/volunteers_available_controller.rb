1e56e3046afa7557e41b40dfc42c5ecaa933f755
mattdvhope <mattagape@me.com>
2017-01-25 12:30:31 +0700

    Added comments

--- app/controllers/volunteers_available_controller.rb
+++ app/controllers/volunteers_available_controller.rb
@@ -5,14 +5,14 @@ class VolunteersAvailableController < ApplicationController
   def index
     volunteers = User.where("users.role = ? OR users.role = ?", "admin", "volunteer")
 
-    vols_with_any_slots = []
+    vols_with_any_slots = [] #eliminate volunteers w/o slots
     volunteers.each do |vol|
       if vol.skype_time_slots.any? 
         vols_with_any_slots.push(vol)
       end
     end
 
-    avail_vols = []
+    avail_vols = [] #eliminate volunteers w/ slots all filled
     vols_with_any_slots.each do |vol|
       vol.skype_time_slots.each do |slot|
         avail_vols.push(vol) if slot.available == true
