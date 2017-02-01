0ff341858757348df7dd932937d3bdc42be7baeb
mattdvhope <mattagape@me.com>
2017-02-01 10:12:10 +0700

    Changed comment

--- app/controllers/volunteers_available_controller.rb
+++ app/controllers/volunteers_available_controller.rb
@@ -25,7 +25,7 @@ class VolunteersAvailableController < ApplicationController
 
   end
 
-  def show # available skype time slots of each individual volunteer
+  def show # available skype time slots of each volunteer
     slots = SkypeTimeSlot.where("skype_time_slots.available = ? OR skype_time_slots.student_id = ?", true, current_user.id)
     @skype_time_slots = slots.where("skype_time_slots.volunteer_id = ?", params[:id])
   end
