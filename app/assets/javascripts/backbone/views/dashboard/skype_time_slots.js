98de3d00b7d95d0f863338cb0701f3a72a1defda
mattdvhope <mattagape@me.com>
2017-02-06 17:23:43 +0700

    Removed some console logs

--- app/assets/javascripts/backbone/views/dashboard/skype_time_slots.js
+++ app/assets/javascripts/backbone/views/dashboard/skype_time_slots.js
@@ -12,24 +12,20 @@ var SkypeTimeSlotsView = Backbone.View.extend({
     promise
     .then(function(collection_objects) {
       return collection_objects.sort(function (a, b) {
-console.log("in then ordertime");
         return a.ordertime - b.ordertime;
       });
     })
     .then(function(collection_objects) {
       return collection_objects.sort(function (a, b) {
-console.log("in then orderam");
         return a.orderam - b.orderam;
       });
     })
     .then(function(collection_objects) {
       return collection_objects.sort(function (a, b) {
-console.log("in then orderday");
         return a.orderday - b.orderday;
       });
     })
     .then(function(collection_objects) {
-console.log(collection_objects);
       $("#time-slot-template").remove();
       $("#list-avail-skype").after(
         view_context.template({
