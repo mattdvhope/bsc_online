c2291e931154556dacca534fb2dcba1c611d3499
mattdvhope <mattagape@me.com>
2017-02-07 16:00:08 +0700

    Added comment

--- app/assets/javascripts/backbone/models/volunteer_available.js
+++ app/assets/javascripts/backbone/models/volunteer_available.js
@@ -5,6 +5,6 @@ var VolunteerAvailable = Backbone.Model.extend({
   //   return attrs;
   // },
 
-  urlRoot: 'volunteers_available'
+  urlRoot: 'volunteers_available' // Rails 'show' here is configured to ONLY provide the JSON of the volunteer's skype_time_slots -- not the user himself!
 
 });
