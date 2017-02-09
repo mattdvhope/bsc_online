84e3c7f6b377d05803654eb19bb669d324ac7c3f
mattdvhope <mattagape@me.com>
2017-02-09 15:50:28 +0700

    Removed comments

--- app/controllers/volunteer_for_student_controller.rb
+++ app/controllers/volunteer_for_student_controller.rb
@@ -4,7 +4,6 @@ class VolunteerForStudentController < ApplicationController
 
   def show
     @volunteer = User.find(params[:id]) # the volunteer (not current_user)
-    # slots = @volunteer.skype_time_slots.where(student_id: current_user.id)
     send_volunteer_email(@volunteer, current_user)
   end
 
@@ -14,8 +13,7 @@ class VolunteerForStudentController < ApplicationController
       if Rails.env.production?
         AppMailer.volunteer_for_student(volunteer, student).deliver_later
       else
-        AppMailer.volunteer_for_student(volunteer, student).deliver_later
-        # AppMailer.development_env_email(volunteer).deliver_later
+        AppMailer.development_env_email(volunteer).deliver_later
       end
     end
 
