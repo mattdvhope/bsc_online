819fc3c5625fc304e09a0a47b6ba680a6f94f4ce
mattdvhope <mattagape@me.com>
2017-02-14 18:54:25 +0700

    Removed comment

--- app/assets/javascripts/backbone/views/dashboard/skype_documents_stu.js
+++ app/assets/javascripts/backbone/views/dashboard/skype_documents_stu.js
@@ -3,11 +3,9 @@ var SkypeDocumentsStuView = Backbone.View.extend({
   template:  HandlebarsTemplates['dashboard/skype_documents_stu'],
 
   render: function() {
-console.log(this.model);
     $("#skype-documents-stu-below").after(
       this.template({
-        student: this.model.toJSON(),
-        // time_slots: collection_objects
+        student: this.model.toJSON()
       })
     );
   } // render
