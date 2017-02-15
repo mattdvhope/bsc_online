d74fb1cac2501d68cfcfa07f237be21653626195
mattdvhope <mattagape@me.com>
2017-02-15 11:19:40 +0700

    Set validations for skype_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -49,6 +49,7 @@ class User < ActiveRecord::Base
   validates_presence_of :age
   validates_presence_of :phone_number, length: { maximum: 30 }
   validates_presence_of :organization, :if => :non_student?
+  validates_presence_of :skype_name
 
   validates_presence_of :email, length: { maximum: 40 }, :unless => :guest?
   VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
