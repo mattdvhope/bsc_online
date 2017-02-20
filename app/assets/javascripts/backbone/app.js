26d9cca1b48bc90905700ff1f62622eaf44af64c
mattdvhope <mattagape@me.com>
2017-02-20 10:14:35 +0700

    Text edits

--- app/assets/javascripts/backbone/app.js
+++ app/assets/javascripts/backbone/app.js
@@ -68,14 +68,11 @@ var App = {
     this.renderNavBar();
     this.scrollUpToTopOfPage();
     dashboard_page.render();
-    // if (volunteer.get("number_of_slots") == 1) {
-    //   $("#volunteer-welcome").append("<h4 id='current-numbers-slots'>You have currently decided to be available for 1 of your Skype-partner time slots, but you can change/edit that below.</h4>")
-    // }
     if (volunteer.get("number_of_slots") == 0) {
       $("#volunteer-welcome").append("<h4 id='current-numbers-slots'>You have currently decided to be available for 0 Skype-partner time slots, but you can change/edit that below.</h4>")
     }
     else {
-      $("#volunteer-welcome").append("<h4 id='current-numbers-slots'>You have currently decided to be available for " + volunteer.get("number_of_slots") + " of your Skype-partner time slots, but you can change/edit that below.</h4>")
+      $("#volunteer-welcome").append("<h4 id='current-numbers-slots'>You have currently decided to be available for " + volunteer.get("number_of_slots") + " out of your total number of Skype-partner time slots (below), but you can change/edit that below.</h4>")
     }
     document.title = volunteer.get("first_name") + " " + volunteer.get("last_name");
     var skype_docs_view = new SkypeDocumentsVolView({ model: volunteer });
