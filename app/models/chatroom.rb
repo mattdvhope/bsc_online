2bb5ddf8894444d70c87271e2be5096578ea61ec
mattdvhope <mattagape@me.com>
2017-03-15 12:45:40 +0700

    Set up chatroom and message migrations and relations

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,7 +1,2 @@
 class Chatroom < ApplicationRecord
-
-  has_many :messages, dependent: :destroy
-  has_many :users, through: :messages
-  validates :topic, presence: true, uniqueness: true, case_sensitive: false
-
 end
