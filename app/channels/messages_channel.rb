6a74db5611976b4a10e310eae8cb61f2fb6c0950
mattdvhope <mattagape@me.com>
2017-03-20 14:39:58 +0700

    Added comments and rebroadcast functionality

--- app/channels/messages_channel.rb
+++ app/channels/messages_channel.rb
@@ -3,6 +3,17 @@ class MessagesChannel < ApplicationCable::Channel
     stream_from 'messages' # 'messages' is a channel
   end #MessagesChannel#subscribed streams from our messages broadcast, sending along any new messages as JSON to the client-side subscription function.
 
+# Streams provide the mechanism by which channels route
+# published content (broadcasts) to their subscribers.
+
+
+    # A common use case is to rebroadcast a message sent
+    # by one client to any other connected clients. Not sure
+    # if it works here...
+    def receive(data) #...to rebroadcast....
+      ActionCable.server.broadcast("messages", data)
+    end
+
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
