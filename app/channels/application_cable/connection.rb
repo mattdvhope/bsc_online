4814e29a9439a7ad330ca5e3f5294d08e8c0daa0
mattdvhope <mattagape@me.com>
2017-03-20 13:03:55 +0700

    Added comment

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,11 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+
+    identified_by :current_user # Anything marked as an
+                                # identifier will automatically
+                                # create a delegate by the same
+                                # name on any channel instances
+                                # created off the connection.
+
   end
 end
