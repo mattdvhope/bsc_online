cd82544a774ee6ba0c6ae67a1fe413aa17a7c230
mattdvhope <mattagape@me.com>
2017-03-20 17:04:43 +0700

    Comment

--- app/channels/messages_channel.rb
+++ app/channels/messages_channel.rb
@@ -1,5 +1,9 @@
 class MessagesChannel < ApplicationCable::Channel
   def subscribed # 'subscribed' is invoked when 'App.cable.subscriptions.create' is invoked in 'js/channels/messages.js'
+
+    # Our MessagesChannel instance will respond to the #current_user
+    # method by delegating it to the connection instance.
+
     stream_from 'messages' # 'messages' is a channel
   end #MessagesChannel#subscribed streams from our messages broadcast, sending along any new messages as JSON to the client-side subscription function.
 
