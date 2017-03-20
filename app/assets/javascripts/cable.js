5f8de33dced65b69946b64c705662ccc94b3f039
mattdvhope <mattagape@me.com>
2017-03-20 14:52:41 +0700

    Comment

--- app/assets/javascripts/cable.js
+++ app/assets/javascripts/cable.js
@@ -5,6 +5,11 @@
 //= require_self
 //= require_tree ./channels
 
+
+// Below we'll instantiate our Action Cable consumer on the
+// client-side, telling it to initiate a WebSocket
+// request, and maintain a persistent connection with
+// ws://localhost:3000/cable.
 (function() {
   this.Appp || (this.Appp = {});
 
