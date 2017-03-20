41a84c679cb8ba19090b3a91e3ab27858b23289b
mattdvhope <mattagape@me.com>
2017-03-20 15:05:58 +0700

    Comment

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,5 +1,7 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+  # A connection instance gets instantiated from this class
+  # every time a WebSocket request is accepted by the server. 
 
     identified_by :current_user # Anything marked as an
                                 # identifier will automatically
