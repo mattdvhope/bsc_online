c8fba2bbe69c039bd038e9c78ff5b144655e70ce
mattdvhope <mattagape@me.com>
2017-03-21 14:03:38 +0700

    Commented out stream from

--- app/channels/messages_channel.rb
+++ app/channels/messages_channel.rb
@@ -4,7 +4,7 @@ class MessagesChannel < ApplicationCable::Channel
     # Our MessagesChannel instance will respond to the #current_user
     # method by delegating it to the connection instance.
 
-    stream_from 'messages' # 'messages' is a channel
+    # stream_from 'messages' # 'messages' is a channel
   end #MessagesChannel#subscribed streams from our messages broadcast, sending along any new messages as JSON to the client-side subscription function.
 
 # Streams provide the mechanism by which channels route
@@ -14,9 +14,9 @@ class MessagesChannel < ApplicationCable::Channel
     # A common use case is to rebroadcast a message sent
     # by one client to any other connected clients. Not sure
     # if it works here...
-    def receive(data) #...to rebroadcast....
-      ActionCable.server.broadcast("messages", data)
-    end
+    # def receive(data) #...to rebroadcast....
+    #   ActionCable.server.broadcast("messages", data)
+    # end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
