2bc61b83b2d670aabd5d7210b56b920b8fc32284
mattdvhope <mattagape@me.com>
2017-03-30 10:05:35 +0700

    Commented out stream from

--- app/assets/javascripts/channels/volunteer_removal.js
+++ app/assets/javascripts/channels/volunteer_removal.js
@@ -7,12 +7,43 @@ Appp.volunteer_removal = Appp.cable.subscriptions.create({channel: "VolunteerRem
     console.log("disconnected from ActionCable");
   },
   received: function(data) {
+
+    if (data.available === false) {
+console.log(data);
+      $('li[data-volunteer-id="'+ data.volunteer_id + '"]')
+      .not('li[data-student-id="'+ data.student_id + '"]').remove();
+    }
+    else if  (data.available === true) {
 console.log(data);
 
-    $('li[data-volunteer-id="'+ data.volunteer_id + '"]')
-    .not('li[data-student-id="'+ data.student_id + '"]').remove();
+    //   $("#ul-of-vol-avail").append(
 
-  },
+    //     '<li data-volunteer-id=' + data.volunteer_id + 'data-student-id=' + data.student_id + 
+    //       '<h3>' +
+    //         'ชื่อ: <span style="text-decoration: underline;">' + data.vol_first_name + ' ' + data.vol_last_name + '</span>' +
+    //       '</h3>' +
+    //       '<h4>' + data.vol_gender + '</h4>' +
+    //       '<h4>อายุ: ' + data.vol_age + '</h4>' +
+    //       '<h4>มลรัฐในสหรัฐ: ' + data.vol_province + '</h4>' +
+    //       '<h4 id="choose-skype-time">เลือกเวลาการสนทนาภาษาอังกฤษทาง Skype กับ คุณ ' + data.vol_first_name + '.  ' + data.vol_first_name + ' มีเวลาว่างกับ 1 ใน ' + data.vol_slot_num + ' ช่องนี้เท่านั้น:</h4>' +
 
-});
+    //         '<div id="checkArray">' +
+    //           '<input class="checkers" data-id=' + '"1" ' + 'data-volunteer-id=' + data.volunteer_id + ' type="checkbox">' +
+    //           '<span id="skype-time-slot-available" data-order=' + '1' + '>' + 'Tuesday ' + '12:00น.' + '</span>' +
+    //         '</div>' +
+
+    //       '<h4>หลังจากคุณเลือกช่วงเวลาสำหรับการสนทนาภาษาอังกฤษทาง Skype จาก ' + data.vol_first_name + ' กรุณาคลิ๊กที่ปุ่มสีเขียวนี้ ระบบตอบรับอีเมลอัตโนมัติจะถูกส่งออกไปเพื่อแจ้ง ' + data.vol_first_name + ' ที่คุณต้องการจะเป็นคู่สนทนาทาง Skype ด้วย ' + data.first_name + '</h4>' +
+    //       '<button id="connect-with-volunteer" class="btn btn-success" data-id=' + '"1"' + 'data-firstname=' + data.vol_first_name + ' ' + 'data-lastname=' + data.vol_last_name + '>' +
+    //         'ส่งอีเมลอัตโนมัติถึง ' + data.first_name +
+    //       '</button>' +
+    //       '<hr>' +
+    //     '</li>'
+
+    //   ); // $("#ul-of-vol-avail").append
+
+    } // else if
+
+  }, // received:
+
+}); // Appp.cable.subscriptions.create
 
--- app/channels/volunteer_restoration_channel.rb
+++ app/channels/volunteer_restoration_channel.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 class VolunteerRestorationChannel < ApplicationCable::Channel
   def subscribed
-    stream_from 'volunteer_restoration'
+    # stream_from 'volunteer_restoration'
   end
 
   def unsubscribed
