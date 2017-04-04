47799afb7d6295764bda07e90da404a6acb60f9f
mattdvhope <mattagape@me.com>
2017-04-04 13:54:55 +0700

    Removed some fields and validations for the student application

--- app/models/user.rb
+++ app/models/user.rb
@@ -53,13 +53,13 @@ class User < ActiveRecord::Base
   validates_presence_of :first_name, length: { maximum: 30 }
   validates_presence_of :last_name, length: { maximum: 40 }
   validates_presence_of :gender
-  validates_presence_of :age
+  # validates_presence_of :age
   # validates_presence_of :phone_number, length: { maximum: 30 }, :unless => :non_student?
   validates_presence_of :organization, :if => :non_student?
   validates_presence_of :skype_name, :unless => :guest?
 
     # check out 'case_sensitive: false' for email...maybe use...
-  validates_presence_of :email, length: { maximum: 40 }, :unless => :guest?
+  validates_presence_of :email, length: { maximum: 40 }, :if => :non_student?
   VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
   validates_format_of :email, :with => VALID_EMAIL_REGEX, :on => :create, :allow_blank => true
   validates_uniqueness_of :email, :allow_blank => true
