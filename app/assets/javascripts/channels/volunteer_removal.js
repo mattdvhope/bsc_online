6f8676228ee223bf5105e75c43f140ee7d0ae46a
mattdvhope <mattagape@me.com>
2017-05-11 14:24:03 +0700

    Fixed returning volunteer's skype time slots

--- app/assets/javascripts/channels/volunteer_removal.js
+++ app/assets/javascripts/channels/volunteer_removal.js
@@ -20,8 +20,6 @@ console.log("false: ", present_user_id);
       .not('li[data-student-id="'+ current_clicker_id + '"]').remove();
     }
     else if (data.available === true) {
-// console.log("true: ", present_user_id);
-console.log(data);
       $('h4[data-id=' + data.volunteer_id + ']').children().remove();
 
       if ($('li[data-volunteer-id=' + data.volunteer_id + ']').length === 0) {
@@ -39,7 +37,7 @@ console.log(data);
             '<h4>มลรัฐในสหรัฐ: ' + data.vol_province + '</h4>' +
             '<h4 id="choose-skype-time" data-id="' + data.volunteer_id + '">เลือกเวลาการสนทนาภาษาอังกฤษทาง Skype กับ คุณ ' + data.vol_first_name + '.  ' + data.vol_first_name + ' มีเวลาว่างกับ 1 ใน ' + data.vol_slot_num + ' ช่องนี้เท่านั้น:</h4>' +
               // slots appended here
-            '<h4>หลังจากคุณเลือกช่วงเวลาสำหรับการสนทนาภาษาอังกฤษทาง Skype จาก ' + data.vol_first_name + ' กรุณาคลิ๊กที่ปุ่มสีเขียวนี้ ระบบตอบรับอีเมลอัตโนมัติจะถูกส่งออกไปเพื่อแจ้ง ' + data.vol_first_name + ' ที่คุณต้องการจะเป็นคู่สนทนาทาง Skype ด้วย ' + data.first_name + '</h4>' +
+            '<h4>หลังจากคุณเลือกช่วงเวลาสำหรับการสนทนาภาษาอังกฤษทาง Skype จาก ' + data.vol_first_name + ' กรุณาคลิ๊กที่ปุ่มสีเขียวนี้ ระบบตอบรับอีเมลอัตโนมัติจะถูกส่งออกไปเพื่อแจ้ง ' + data.vol_first_name + ' ที่คุณต้องการจะเป็นคู่สนทนาทาง Skype ด้วย ' + data.vol_first_name + '</h4>' +
             '<button id="connect-with-volunteer" class="btn btn-success" data-id=' + '"1"' + 'data-firstname=' + data.vol_first_name + ' ' + 'data-lastname=' + data.vol_last_name + '>' +
               'ส่งอีเมลอัตโนมัติถึง ' + data.vol_first_name +
             '</button>' +
@@ -56,15 +54,15 @@ console.log(data);
         $('h4[data-id=' + data.volunteer_id + ']').append(
           '<div id="checkArray">' +
             '<input class="checkers" data-id=' + slot.id + ' data-volunteer-id=' + data.volunteer_id + ' data-student-id=' + present_user_id + ' type="checkbox" style="transform: scale(1.5, 1.5); zoom: 1.2; margin-left: 0.8em; margin-top: 0.3em">' +
-            '<span id="skype-time-slot-available" style="font-size: 16.5px;" data-order=' + '1' + '>' + slot.day_thai + ' ' + slot.time_thai + '</span>' +
+            '<span id="skype-time-slot-available" style="font-size: 16.5px;" data-order=' + slot.order_day + '>' + slot.day_thai + ' ' + slot.time_thai + '</span>' +
           '</div>'
-        ); // 'h4[data-id='... .append
+        );
       });
 
-      if ($('span:contains("กับคุณ")').length > 0) {
+      if ($('span:contains("น. กับคุณ")').length > 0) {
         $(".checkers[data-volunteer-id=" + data.volunteer_id + "][data-student-id=" + present_user_id + "]").parent().find('input').each(function() {
-          console.log($(this)[0]);
           $(this).attr("disabled", true);
+          $(this).next().css( "color", "#b0b8c4" )
         });
       }
 
