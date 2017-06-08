bcd1d1cf36223ed95c71ea9984351e2b02a77c2e
mattdvhope <mattagape@me.com>
2017-06-08 09:51:41 +0700

    Continuing work on off site locations on dashboard

--- app/assets/javascripts/backbone/views/application_form/off_site_locations.js
+++ app/assets/javascripts/backbone/views/application_form/off_site_locations.js
@@ -1,22 +1,31 @@
 var OffSiteLocationsView = Backbone.View.extend({
 
   initialize: function() {
-    this.$el.appendTo(".entire");
+    if (!this.model) {
+      this.$el.appendTo(".entire");
+    }
   },
 
-  template:  HandlebarsTemplates['application_form/off_site_locations'],
+  template_dash: HandlebarsTemplates['dashboard/off_site_locations'],
+
+  template_appl: HandlebarsTemplates['application_form/off_site_locations'],
+
+  handlebars_data: function() {
+    return {
+      thai_language: thai_language(),
+      off_site_locations: this.collection
+    };
+  },
 
   render: function() {
-
     if (this.model) {
-      console.log("model here");
+      $('.dash-create-classes').after(this.template_dash(handlebars_data()));
     } else {
-      $("#off-site-fields").html(this.template({
+      $("#off-site-fields").html(this.template_appl({
         thai_language: thai_language(),
         off_site_locations: this.collection
       }));
     }
-
     return this;
   }
 
