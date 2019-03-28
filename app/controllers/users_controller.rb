d1a7b59b86d1ca2317b005624d5533e7550d558a
mattdvhope <mattagape@me.com>
2019-03-28 18:46:20 +0700

    Edited user registration process

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,93 +11,23 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def register_admin
-    redirect_to volunteer_info_path
-  end
-
-  def register_vol
-    redirect_to volunteer_info_path
-  end
-
   def new
     redirect_to root_path
   end
 
-  def new_admin
-    @user = User.new
-  end
-
   def create
     user = User.new(user_params)
-    user.first_name = user.first_name.downcase.capitalize
-    user.last_name = user.last_name.downcase.capitalize
-    user.off_site_location_id = user.off_site_location_id.to_i
-    if user.phone_number
-      user.phone_number = user.phone_number.gsub(/(?!^\+)\D*/, '')
-    end
-    user.email = user.email.downcase
+    format_user_attributes(user)
     log_out_path if users_path
-    if user.guest
-      user.nickname = user.nickname.downcase.capitalize
-      deal_with_guest(user)
-    elsif user.role == "volunteer" && user.pin != "000000"
-      user.nickname = "vol"
-      deal_with_volunteer(user)
-    elsif user.role == "admin_applicant"
-      user.nickname = "adm"
-      deal_with_admin_applicant(user)
-    elsif user.role == "student" && user.pin != "000000"
-      user.nickname = user.nickname.downcase.capitalize
-      fully_register_student(user)
-    end
+    deal_with_student(user)
   end
 
   def update
-    @user = User.find(params[:id])
-    if params[:number_of_slots]
-      @user.number_of_slots = params[:number_of_slots]
-      add_number_of_slots_to_volunteer(@user)
-    else
-      @user.update_attributes(user_params)
-      render :nothing => true
-    end
-  end
-
-  def add_number_of_slots_to_volunteer(user)
-    if user.save!(:validate => false)
-      render "show"
-    else
-      render :json => { :errors => user.errors.full_messages }, :status => 422
-    end
-  end
-
-  def approve_admin
-    user = User.find(params[:id])
-    if current_user.leader?
-      user.role = "admin"
-      user.guest = false
-      user.save!(:validate => false)
-    end
-    render :nothing => true
-  end
-
-  def disapprove_admin
-    user = User.find(params[:id])
-    if current_user.leader?
-      user.role = "disapproved_admin"
-      # user.guest = true
-      user.save!(:validate => false)
-    end
-    render :nothing => true
-  end
-
-  def unsubscribe
-    if user = User.read_access_token(params[:signature])
-      user.update_attribute :guest, false # maybe add a boolean attribute, "email_opt_in" or something like that... this attribute would be made false here
-      render text: "คุณได้รับการยกเลิกการเป็นสมาชิก / You have been unsubscribed."
-    else
-      render text: "Invalid Link"
-    end
+    phone_number = user_params[:phone_number]
+    phone_number = phone_number.gsub(/(?!^\+)\D*/, '')
+    user = User.find_by(phone_number: phone_number)
+    user.class_times << ClassTime.where(id: params[:user][:trainingPeriodId].to_i)
+    render json: nil, status: :ok
   end
 
   private
@@ -106,14 +36,19 @@ class UsersController < ApplicationController
       params.require(:user).permit(:nickname, :first_name, :last_name, :image, :email, :facebook, :line, :skype_name, :number_of_slots, :password, :password_confirmation, :postal_code, :address_1, :address_2, :city, :sub_district, :district, :province, :country, :phone_number, :organization, :age, :gender, :guest, :role, :pin, :off_site_location_id)
     end
 
-    def deal_with_guest(user)
-      # previous_user = User.where(phone_number: user.phone_number)[0]
-      # previous_user ? user = previous_user : user = user
-      # user.date_format = DateTime.now.strftime("%A, %B %d, %Y")
+    def format_user_attributes(user)
+      user.first_name = user.first_name.downcase.capitalize
+      user.last_name = user.last_name.downcase.capitalize
+      user.off_site_location_id = user.off_site_location_id.to_i
+      user.phone_number = user.phone_number.gsub(/(?!^\+)\D*/, '')
+      user.email = user.email.downcase
+      user.nickname = user.nickname.downcase.capitalize
+      user
+    end
 
-
+    def deal_with_student(user)
       if user.save
-        user.class_times << ClassTime.where(id: user.off_site_location_id.to_i)
+        user.class_times << ClassTime.where(id: params[:user][:trainingPeriodId].to_i)
         render json: {:code=>200, :message=>"Successful creation of new user!!"}
       else
         begin
@@ -124,121 +59,12 @@ class UsersController < ApplicationController
           render json: nil, status: :ok
         end
       end
-
-
-#       if !previous_user # if NOT a previous user
-#         partic = CepParticipation.new
-#         partic.class_time = ClassTime.find_by(period: params[:class_time_scheduled_1])
-#         if user.save
-#           partic.user = user
-#           partic.save
-#           render json: nil, status: :ok # to render nothing, but still retain json response; It did cause a problem with 'parse in user.js though'.. have to check it out
-#           if user.email != ""
-#             send_new_user_email(user)
-#           end
-#         else
-# binding.pry
-#           render :json => { :errors => user.errors.full_messages }, :status => 422
-#         end
-
-#       else # if this IS a previous user...
-#         if previous_user.cep_participations.count > 2
-# binding.pry
-#           render :json => { :errors => previous_user.errors.full_messages }, :status => 422
-#         elsif params[:class_time_scheduled_2] == "select_option"
-# binding.pry
-#           render :json => { :errors => previous_user.errors.full_messages }, :status => 422
-#         else
-# binding.pry
-#           partic = CepParticipation.new
-#           partic.class_time = ClassTime.find_by(period: params[:class_time_scheduled_2])
-#           partic.user = previous_user
-#           partic.save
-#           previous_user.save!(:validate => false)
-#           if previous_user.email != ""
-#             send_new_user_email(previous_user)
-#           end
-#           render json: nil, status: :ok # to render nothing, but still retain json response; It did cause a problem with 'parse in user.js though'.. have to check it out
-#         end
-#       end
-
-    end
-
-    def fully_register_student(user)
-      if (User.pins_available =~ user.pin) == 0
-        guests = User.where("users.role = ?", "student").where("users.guest = ?", "TRUE")
-        old_guest_student = guests.find_by(email: user.email.downcase)
-        if old_guest_student
-          old_guest_student.guest = false
-          set_password(old_guest_student)
-          old_guest_student.skype_name = user.skype_name
-          if old_guest_student.save
-            student_render(old_guest_student)
-            send_new_user_email(old_guest_student)
-          else
-            render :json => { :errors => old_guest_student.errors.full_messages }, :status => 422
-          end
-        else
-          set_password(user)
-          if user.save
-
-#             auth_token = AuthenticateUser.new(user.email, user.password).call
-#             response = { message: Message.account_created, auth_token: auth_token }
-#             json_response(response, :created)
-
-            student_render(user)
-            send_new_user_email(user)
-          else
-            render :json => { :errors => user.errors.full_messages }, :status => 422
-          end
-        end
-      else
-        deal_with_bad_pin
-      end
-    end #
-
-    def set_password(user)
-      user.password = params[:password]
-      user.password_confirmation = params[:password_confirmation]
     end
 
     def student_render(user)
       session[:user_id] = user.id
       @user = user
       render "show" # to get JSON in jbuilder
-    end
-
-    def deal_with_volunteer(user)
-      if (User.pins_available =~ user.pin) == 0
-        save_vol_or_admin_applicant(user)
-      else
-        deal_with_bad_pin
-      end
-    end
-
-    def deal_with_bad_pin
-      render :json => { :errors => "Incorrect PIN" }, :status => 422
-    end
-
-    def deal_with_admin_applicant(user)
-      save_vol_or_admin_applicant(user)
-    end
-
-    def save_vol_or_admin_applicant(user)
-      deal_with_password(user)
-      if user.save
-        session[:user_id] = user.id unless user.role == "admin_applicant"
-        send_new_user_email(user)
-        @user = user
-        render json: nil, status: :ok # to get JSON in jbuilder
-      else
-        render :json => { :errors => user.errors.full_messages }, :status => 422
-      end
-    end
-
-    def deal_with_password(user)
-      user.password = params[:password]
-      user.password_confirmation = params[:password_confirmation]       
     end
 
     def send_new_user_email(user)
